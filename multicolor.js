f766ffd466b2c47ffe4cc2599b06668d90d405fa
erika <erika@erikasf.com>
2015-05-14 18:46:30 -0700

    Update and rename multicolor.js to multicolorAudio.js

--- multicolor.js
+++
@@ -1,9 +0,0 @@
-for (var i=0; i<81; i++) {
-  var newDiv =
-	 document.createElement("div");
-	newDiv.style.width = "11.1%";
-	newDiv.style.float = "left";
-	newDiv.style.paddingBottom = "11.1%";
-	newDiv.style.backgroundColor ='#'+Math.floor(Math.random()*16777215).toString(16);
-	document.body.appendChild(newDiv);
-};
