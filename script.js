a5532c515a7e46702b7cba404d4a90750d8c6a34
Kenny Zhang <Kenny.y.zhang@gmail.com>
2015-08-17 14:54:15 -0700

    adding work

--- script.js
+++ script.js
@@ -1,8 +1,10 @@
+
 window.onload = function(){
 	makeDivs();
+	changeColor();
 };
 
-function makeDivsGradient(){
+function makeDivs(){
 	var parent = document.getElementsByTagName("body")[0];
 	for (var i = 0; i<100; i++){
 		var div = document.createElement("div");
@@ -10,7 +12,29 @@ function makeDivsGradient(){
 		div.style.float="left";
 		div.style.paddingBottom="11.1%";
 		div.style.height="11.1%";
-		div.style.backgroundColor="rgba(255,0,0, "+(i/100)+")";
+		div.style.backgroundColor=getRandomColor();
 		parent.appendChild(div);
 	}
+}
+
+function getRandomColor() {
+    var letters = '0123456789ABCDEF'.split('');
+    var color = '#';
+    for (var i = 0; i < 6; i++ ) {
+        color += letters[Math.floor(Math.random() * 16)];
+    }
+    return color;
+}
+
+function goThroughDivs(){
+	//for each div, set color
+	var divs = document.getElementsByTagName("div");
+
+	for (var i = 0; i<divs.length; i++){
+		divs[i].style.backgroundColor=getRandomColor();
+	}
+}
+
+function changeColor(){
+	setInterval(goThroughDivs,2000);
 }
