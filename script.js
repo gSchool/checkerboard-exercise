103520aaf6f6c551f7ad9a37b38941ed43a44642
Kenny Zhang <Kenny.y.zhang@gmail.com>
2015-08-17 14:22:22 -0700

    adding gradient

--- script.js
+++ script.js
@@ -1,1 +1,16 @@
-// Your JS goes here
+window.onload = function(){
+	makeDivs();
+};
+
+function makeDivsGradient(){
+	var parent = document.getElementsByTagName("body")[0];
+	for (var i = 0; i<100; i++){
+		var div = document.createElement("div");
+		div.style.width="11.1%";
+		div.style.float="left";
+		div.style.paddingBottom="11.1%";
+		div.style.height="11.1%";
+		div.style.backgroundColor="rgba(255,0,0, "+(i/100)+")";
+		parent.appendChild(div);
+	}
+}
