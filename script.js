07062d90153b5c092ca1f946ad92fc79b7376e77
Michael Yeager <m.yeager001@gmail.com>
2015-11-03 14:19:10 -0700

    add checkerboard js

--- script.js
+++ script.js
@@ -1,1 +1,12 @@
-// Your JS goes here
+// created the checkerboard
+for(var i = 0; i < 81; i++){
+  var divCreate = document.createElement('div');
+  console.log(divCreate);
+  if(i%2){
+    divCreate.style.backgroundColor = 'red';
+  }else{divCreate.style.backgroundColor = 'black';}
+  divCreate.style.width = '11.1%';
+  divCreate.style.cssFloat = 'left'
+  divCreate.style.paddingBottom = '11.1%'
+  document.body.appendChild(divCreate);
+}
