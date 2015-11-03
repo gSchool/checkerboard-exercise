31cd6a6f06692c5e285b27e881a67fc66287b5f6
Michael Yeager <m.yeager001@gmail.com>
2015-11-03 14:39:34 -0700

    add random color to checkerboard

--- script.js
+++ script.js
@@ -1,10 +1,25 @@
 // created the checkerboard
+// for(var i = 0; i < 81; i++){
+//   var divCreate = document.createElement('div');
+//   console.log(divCreate);
+//   if(i%2){
+//     divCreate.style.backgroundColor = 'red';
+//   }else{divCreate.style.backgroundColor = 'black';}
+//   divCreate.style.width = '11.1%';
+//   divCreate.style.cssFloat = 'left'
+//   divCreate.style.paddingBottom = '11.1%'
+//   document.body.appendChild(divCreate);
+// }
+
 for(var i = 0; i < 81; i++){
   var divCreate = document.createElement('div');
   console.log(divCreate);
+  var R = Math.floor(Math.random()*360);
+  var G = Math.floor(Math.random()*360);
+  var B = Math.floor(Math.random()*360);
   if(i%2){
-    divCreate.style.backgroundColor = 'red';
-  }else{divCreate.style.backgroundColor = 'black';}
+    divCreate.style.backgroundColor = 'rgb('+R+','+G+','+B+')'
+  }else{divCreate.style.backgroundColor = 'rgb('+R+','+G+','+B+')'}
   divCreate.style.width = '11.1%';
   divCreate.style.cssFloat = 'left'
   divCreate.style.paddingBottom = '11.1%'
