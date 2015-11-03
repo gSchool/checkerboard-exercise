7def8b07cda02d919c68ba94c273c657515c8f5a
Jonathan Casey <jon_casey@me.com>
2015-11-03 12:11:13 -0700

    jcasey solution for checkerboard exercise

--- script.js
+++ script.js
@@ -1,1 +1,16 @@
-// Your JS goes here
+for(var i = 0; i < 81 ; i ++){
+  var square  = document.createElement('div');
+  square.style.width = "11.1%";
+  square.style.float = "left"
+  square.style.margin = "0";
+  square.style.paddingBottom = "11.1%";
+  if(i % 2 === 0){
+    square.style.backgroundColor = "black";
+  }else{
+    square.style.backgroundColor = "red";
+
+  }
+var body = document.getElementsByTagName('body')[0];
+body.style.margin = "0"
+document.body.appendChild(square);
+}
