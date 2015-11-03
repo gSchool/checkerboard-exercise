e0b17bf08d238aea79ca959a8e133580f6b3c7a1
Zachary Smith <billyzacsmith@gmail.com>
2015-11-03 12:21:19 -0700

    Add divs. Style em. Almost done.

--- script.js
+++ script.js
@@ -1,6 +1,14 @@
-// Add the divs to the DOM
+var body = document.getElementsByTagName('body')[0]
+body.style.backgroundColor = 'aqua'
 
-// Style the divs
-  // height and width
-  // layout: float left, padding, margin
-  // color
+for (var i=0; i < 81; i++) {
+  var square = document.createElement('div')
+  square.style.width = '11.1%'
+  square.style.paddingBottom = '11.1%'
+  square.style.backgroundColor = 'gray'
+  if (i%2 === 0) {
+    square.style.backgroundColor = 'tomato'
+  }
+  square.style.float = 'left'
+  body.appendChild(square)
+}
