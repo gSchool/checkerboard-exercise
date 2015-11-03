b5d7fc89d58d3c7804aaaa4c8bd54aa7cd506197
Ben Allred <skuttleman@gmail.com>
2015-11-03 12:38:03 -0700

    Made background color random

--- script.js
+++ script.js
@@ -1,4 +1,8 @@
 // Your JS goes here
+function ran() {
+  return Math.floor(Math.random() * 256);
+}
+
 var body = document.getElementsByTagName('body')[0];
 body.style.margin = '0';
 for (var i = 0; i < 9 * 7; i ++)
@@ -6,6 +10,7 @@ for (var i = 0; i < 9 * 7; i ++)
   var div = document.createElement('div');
   div.style.width = div.style.paddingBottom = '11.1%';
   div.style.float = 'left';
-  div.style.backgroundColor = i % 2 === 0 ? 'black' : 'red';
+  div.style.backgroundColor =
+    'rgb(' + [ran(), ran(), ran()].join(', ') + ')';
   body.appendChild(div);
 }
