c79f6ba5c6ce33c5819ff950f333745db6a2621b
Ash Kestrel <tetrapteryxgames@gmail.com>
2015-11-03 14:43:15 -0700

    Made it FABULOUS

--- script.js
+++ script.js
@@ -1,5 +1,11 @@
 document.body.style.margin = '0';
 document.body.style.overflow = 'hidden';
+var newAudio = document.createElement('audio');
+document.body.appendChild(newAudio);
+var newSource = document.createElement('source');
+newAudio.appendChild(newSource);
+newSource.src = 'audio/music.mp3';
+newAudio.play();
 function randomHex(){
   var hex = ['0','1','2','3','4','5','6','7','8','9','A','B','C','D','E','F'];
   var color = '#'
@@ -11,12 +17,35 @@ function addChecker(n){
   while(n>0) {
     var newDiv = document.createElement('div');
     document.body.appendChild(newDiv);
-    newDiv.style.width = '11.1%';
+    var raptor = document.createElement('img');
+    raptor.src = "images/raptor.png";
+    newDiv.appendChild(raptor);
+    newDiv.style.width = '7.1%';
     newDiv.style.float = 'left';
-    newDiv.style.paddingBottom = '11.1%';
+    newDiv.style.padding = '2%';
     newDiv.style.backgroundColor = randomHex();
+    newDiv.style.textAlign = 'center';
     n--;
   }
 }
-
+function flipRaptor(i){
+  if (raptorDir === 0){
+    allRaptors[i].style.transform = "rotateY(180deg)";
+  } else {
+    allRaptors[i].style.transform = "rotateY(0deg)";
+  }
+}
 addChecker(100);
+var allCheckers = document.getElementsByTagName('div');
+var allRaptors = document.getElementsByTagName('img');
+var raptorDir = 0;
+setInterval(function(){
+  for (var i=0;i<allCheckers.length;i++){
+    allCheckers[i].style.backgroundColor = randomHex();
+    flipRaptor(i);
+  }
+  if (raptorDir)
+    raptorDir = 0;
+  else
+    raptorDir = 1;
+}, 700);
