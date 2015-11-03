40d31193044ed9828fa3d7c11600ce47ce0c182c
Isaac Lessard <IsaacL@Isaacs-MacBook-Air-2.local>
2015-11-03 11:59:51 -0700

    finished checkers

--- script.js
+++ script.js
@@ -1,1 +1,20 @@
 // Your JS goes here
+document.body.style.padding="0";
+document.body.style.margin="0";
+
+
+for(i=0;i<108;i++){
+  var box = document.createElement('div');
+  box.style.width = '11.1vw';
+  box.style.padding = '0';
+  box.style.margin = '0';
+  box.style.height = '11.1vh';
+  box.style.float = "left";
+
+  if(i%2===0){
+    box.style.backgroundColor = "black";
+  } else {
+    box.style.backgroundColor = "red";
+  }
+  document.body.appendChild(box);
+}
