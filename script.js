ac66ec00c893739ffb3d0f086b689fa05654bc18
Ash Kestrel <tetrapteryxgames@gmail.com>
2015-11-03 13:10:05 -0700

    Made colors random

--- script.js
+++ script.js
@@ -1,6 +1,12 @@
 document.body.style.margin = '0';
 document.body.style.overflow = 'hidden';
-var check = 0;
+function randomHex(){
+  var hex = ['0','1','2','3','4','5','6','7','8','9','A','B','C','D','E','F'];
+  var color = '#'
+  while(color.length < 7)
+    color += hex[Math.floor(Math.random() * 16)];
+  return color;
+}
 function addChecker(n){
   while(n>0) {
     var newDiv = document.createElement('div');
@@ -8,13 +14,7 @@ function addChecker(n){
     newDiv.style.width = '11.1%';
     newDiv.style.float = 'left';
     newDiv.style.paddingBottom = '11.1%';
-    if (check === 0) {
-      newDiv.style.backgroundColor = 'red';
-      check = 1;
-    } else {
-      newDiv.style.backgroundColor = 'black';
-      check = 0;
-    }
+    newDiv.style.backgroundColor = randomHex();
     n--;
   }
 }
