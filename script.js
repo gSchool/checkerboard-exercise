b67ae6a9b012d8fc7109baee45e557f378e96902
Ash Kestrel <tetrapteryxgames@gmail.com>
2015-11-03 13:33:17 -0700

    Made gradient

--- script.js
+++ script.js
@@ -1,7 +1,9 @@
 document.body.style.margin = '0';
 document.body.style.overflow = 'hidden';
 var check = 0;
-function addChecker(n){
+var colors1 = ['#B7FF6E','#ADFF5B','#A3FF47','#99FF33','#89FF14','#7AF400','#6AD400','#5BB500','#4B9600'];
+var colors2 = ['#B76EFF','#AD5BFF','#A347FF','#9933FF','#8914FF','#7A00F4','#6A00D4','#5B00B5','#4B0096'];
+function addChecker(n,row){
   while(n>0) {
     var newDiv = document.createElement('div');
     document.body.appendChild(newDiv);
@@ -9,14 +11,15 @@ function addChecker(n){
     newDiv.style.float = 'left';
     newDiv.style.paddingBottom = '11.1%';
     if (check === 0) {
-      newDiv.style.backgroundColor = 'red';
+      newDiv.style.backgroundColor = colors1[row];
       check = 1;
     } else {
-      newDiv.style.backgroundColor = 'black';
+      newDiv.style.backgroundColor = colors2[row];
       check = 0;
     }
     n--;
   }
 }
-
-addChecker(100);
+for (i=0;i<9;i++){
+  addChecker(9, i);
+}
