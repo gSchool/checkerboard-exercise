eb56a0d3fc97787c9df9f2da07528987a9de509e
Ryan Douglas <ryan@luckydogdigital.com>
2015-11-03 13:21:23 -0700

    basics in place

--- script.js
+++ script.js
@@ -1,1 +1,27 @@
 // Your JS goes here
+
+// var board = function () {
+//   this.width = width;
+//   this.height = height;
+// }
+
+// var width = prompt("How wide is the board?");
+// var height = prompt("How tall is the board?");
+// var counter = 0;
+
+var board = function(height) {
+var body = document.getElementsByTagName('body')[0];
+
+  for (var i = 0; i < height * 9; i++) {
+    document.createElement('div');
+    body.appendChild('div')[i];
+    var target = document.getElementsByTagName('div')[i];
+    target.style.width = '11.1%';
+    target.style.paddingBottom = '11.1%';
+    target.style.float = 'left';
+    target.style.border = '1px solid black';
+
+      }
+    };
+
+board(3);
