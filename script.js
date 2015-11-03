265b4ab2f6d924a583210b5a969505f551a39728
Isaac Lessard <IsaacL@Isaacs-MacBook-Air-2.local>
2015-11-03 12:02:33 -0700

    solved % bug

--- script.js
+++ script.js
@@ -1,14 +1,16 @@
 // Your JS goes here
 document.body.style.padding="0";
 document.body.style.margin="0";
+document.body.style.width="100vw";
+document.body.style.height="100vh";
 
 
 for(i=0;i<108;i++){
   var box = document.createElement('div');
-  box.style.width = '11.1vw';
+  box.style.width = '11.1%';
   box.style.padding = '0';
   box.style.margin = '0';
-  box.style.height = '11.1vh';
+  box.style.height = '11.1%';
   box.style.float = "left";
 
   if(i%2===0){
