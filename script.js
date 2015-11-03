6ed74a540dd084601b8670710d8fc2bc94dbe773
Ben Allred <skuttleman@gmail.com>
2015-11-03 12:34:46 -0700

    Resolved to use divs instead of table

--- script.js
+++ script.js
@@ -1,19 +1,11 @@
 // Your JS goes here
-var table = document.createElement('table');
-table.style.borderCollapse = 'collapse';
-var size = window.innerWidth / 9;
-
-for (var i = 0; i < 7; i ++) {
-  var tr = document.createElement('tr');
-  for (var j = 0; j < 9; j ++) {
-    var td = document.createElement('td');
-    td.style.height = td.style.width = size + 'px';
-    td.style.backgroundColor = ((j + i) % 2 === 0) ? 'black' : 'red';
-    tr.appendChild(td);
-  }
-  table.appendChild(tr);
-}
-
 var body = document.getElementsByTagName('body')[0];
-body.appendChild(table);
 body.style.margin = '0';
+for (var i = 0; i < 9 * 7; i ++)
+{
+  var div = document.createElement('div');
+  div.style.width = div.style.paddingBottom = '11.1%';
+  div.style.float = 'left';
+  div.style.backgroundColor = i % 2 === 0 ? 'black' : 'red';
+  body.appendChild(div);
+}
