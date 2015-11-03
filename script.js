35751a9da1c54eeefb9b05e7297f87f92f4c9c66
Ash Kestrel <tetrapteryxgames@gmail.com>
2015-11-03 12:57:36 -0700

    Made checkerboard

--- script.js
+++ script.js
@@ -1,1 +1,22 @@
-// Your JS goes here
+document.body.style.margin = '0';
+document.body.style.overflow = 'hidden';
+var check = 0;
+function addChecker(n){
+  while(n>0) {
+    var newDiv = document.createElement('div');
+    document.body.appendChild(newDiv);
+    newDiv.style.width = '11.1%';
+    newDiv.style.float = 'left';
+    newDiv.style.paddingBottom = '11.1%';
+    if (check === 0) {
+      newDiv.style.backgroundColor = 'red';
+      check = 1;
+    } else {
+      newDiv.style.backgroundColor = 'black';
+      check = 0;
+    }
+    n--;
+  }
+}
+
+addChecker(100);
