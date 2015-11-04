367fbab84f3c224d1352dfaf542a1048957f5733
Jennifer Bennett <jennbbennett@hotmail.com>
2015-11-03 20:49:52 -0700

    finished checkerboard pattern with red and black

--- script.js
+++ script.js
@@ -1,1 +1,19 @@
 // Your JS goes here
+
+var body = document.getElementsByTagName('body')[0];
+
+var i = 0;
+ while (i<63) {
+ var tile  = document.createElement('div');
+  tile.style.width = "11.1%";
+  tile.style.float = "left"
+  tile.style.margin = "0";
+  tile.style.paddingBottom = "11.1%";
+  if(i % 2 === 0){
+    tile.style.backgroundColor = "black";
+  } else {
+    tile.style.backgroundColor = "red";
+  }
+  document.body.appendChild(tile);
+  i++;
+}
