ae360ad76b4ce358073f68a8e27421132f3ec2f9
Jennifer Bennett <jennbbennett@hotmail.com>
2015-11-03 21:17:22 -0700

    added random color generator

--- script.js
+++ script.js
@@ -5,15 +5,12 @@ var body = document.getElementsByTagName('body')[0];
 var i = 0;
  while (i<63) {
  var tile  = document.createElement('div');
+ var randomColor = "#" + Math.floor(Math.random()*16777215).toString(16);
   tile.style.width = "11.1%";
   tile.style.float = "left"
   tile.style.margin = "0";
   tile.style.paddingBottom = "11.1%";
-  if(i % 2 === 0){
-    tile.style.backgroundColor = "black";
-  } else {
-    tile.style.backgroundColor = "red";
-  }
+  tile.style.backgroundColor = randomColor;
   document.body.appendChild(tile);
   i++;
 }
