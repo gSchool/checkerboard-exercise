b7bd476766fa1901fbfcf2f60ac32e1c764ec776
Ryan Douglas <ryan@luckydogdigital.com>
2015-11-04 17:40:18 -0700

    finished

--- script.js
+++ script.js
@@ -1,27 +1,66 @@
 // Your JS goes here
+document.body.style.height = "100vh";
+document.body.style.width = "100vw";
+document.body.style.padding = "0";
+document.body.style.margin = "0";
+document.body.style.border = "1px solid black";
 
-// var board = function () {
-//   this.width = width;
-//   this.height = height;
-// }
+var hypno = document.createElement('img');
+hypno.src = "http://sukhanov.net/images/hypnotoad.svg";
+hypno.style.height = "200px";
+hypno.style.width = "200px";
+hypno.style.marginTop = "25%";
+hypno.style.marginLeft = "50%";
+hypno.style.top = "-100px";
+hypno.style.left = "-100px";
+hypno.style.position = "fixed";
+// hypno.style.zIndex = '99';
 
-// var width = prompt("How wide is the board?");
-// var height = prompt("How tall is the board?");
+var track = document.createElement('audio');
+track.src = "trippy.mp3";
+track.autoplay = "autoplay";
+
+document.body.appendChild(hypno);
+document.body.appendChild(track);
+
+
+for (var i = 0; i < 36; i++) {
+
+  var target = document.createElement('div');
+  target.style.width = '11.1%';
+  target.style.float = 'left';
+  target.style.paddingBottom = '13%';
+  target.style.margin = '0';
+
+  var r = Math.floor(Math.random() * 256).toString();
+  var g = Math.floor(Math.random() * 256).toString();
+  var b = Math.floor(Math.random() * 256).toString();
+
+  target.style.background = 'rgb('+ r +','+ g + ',' + b +' )';
+  document.body.appendChild(target);
+}
+
+var div = document.getElementsByTagName('div');
+console.log(div);
+
+
+
+var interval;
+
+function cycle() {
+  interval = setInterval(changeColor, 100);
+}
+
 // var counter = 0;
+var changeColor = function() {
+  for (var i = 0; i < div.length; i++) {
+    var target = div[i];
+    var r = Math.floor(Math.random() * 256).toString();
+    var g = Math.floor(Math.random() * 256).toString();
+    var b = Math.floor(Math.random() * 256).toString();
 
-var board = function(height) {
-var body = document.getElementsByTagName('body')[0];
+    target.style.background = 'rgb('+ r +','+ g + ',' + b +' )';
+  }
+};
 
-  for (var i = 0; i < height * 9; i++) {
-    document.createElement('div');
-    body.appendChild('div')[i];
-    var target = document.getElementsByTagName('div')[i];
-    target.style.width = '11.1%';
-    target.style.paddingBottom = '11.1%';
-    target.style.float = 'left';
-    target.style.border = '1px solid black';
-
-      }
-    };
-
-board(3);
+cycle();
