e844bf117db17104cfea2c368c39222fdf25256d
Jennifer Bennett <jennbbennett@hotmail.com>
2015-11-04 19:42:10 -0700

    still working

--- script.js
+++ script.js
@@ -1,27 +1,48 @@
 // Your JS goes here
 
-var body = document.getElementsByTagName('body')[0];
-body.style.background = 'linear-gradient(to bottom, #3441FB, #3DC7FC)';
+// var body = document.getElementsByTagName('body')[0];
+// body.style.background = 'linear-gradient(to bottom, #3441FB, #3DC7FC)';
+//
+// var i = 0;
+//  while (i<63) {
+//  var tile  = document.createElement('div');
+//  var randomColor = "#" + Math.floor(Math.random()*16777215).toString(16);
+//   tile.style.width = "11.1%";
+//   tile.style.float = "left"
+//   tile.style.margin = "0";
+//   tile.style.paddingBottom = "11.1%";
+//   tile.style.opacity = "0.5";
+//   if(i % 2 === 0){
+//       tile.style.backgroundColor = "#3DC7FC";
+//     } else {
+//       tile.style.backgroundColor = "#D12DFB";
+//     }
+//     document.body.appendChild(tile);
+//     i++;
+//   }
 
-var i = 0;
- while (i<63) {
- var tile  = document.createElement('div');
- var randomColor = "#" + Math.floor(Math.random()*16777215).toString(16);
-  tile.style.width = "11.1%";
-  tile.style.float = "left"
-  tile.style.margin = "0";
-  tile.style.paddingBottom = "11.1%";
-  tile.style.opacity = "0.5";
-  if(i % 2 === 0){
-      tile.style.backgroundColor = "#3DC7FC";
-    } else {
-      tile.style.backgroundColor = "#D12DFB";
-    }
+  var body = document.getElementsByTagName('body')[0];
+
+  var i = 0;
+   while (i<63) {
+   var tile  = document.createElement('div');
+   //var randomColor = "#" + Math.floor(Math.random()*16777215).toString(16);
+  // var flashingColor = setInterval(randomColor, 2000);
+    tile.style.width = "11.1%";
+    tile.style.float = "left";
+    tile.style.margin = "0";
+    tile.style.paddingBottom = "11.1%";
+    tile.style.backgroundColor = function randomColors(){
+      var randomColor = "#" + Math.floor(Math.random()*16777215).toString(16);
+      return randomColor;
+    };
+
     document.body.appendChild(tile);
     i++;
   }
 
-
-
-
-//background: linear-gradient(top,  #2E2E28 0%,#4D4C48 100%);
+//  setTimeout(randomColors, 2000)
+//setInterval(function() {
+  //  document.body.style.backgroundColor = color(); //() to execute the function!
+//}, 1000);
+//function randomColors(){}
