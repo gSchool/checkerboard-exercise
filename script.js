f77cc875064c354667fca9a1e9dceb1cc41eead0
Jennifer Bennett <jennbbennett@hotmail.com>
2015-11-04 20:31:15 -0700

    completed flashing code

--- script.js
+++ script.js
@@ -1,48 +1,31 @@
 // Your JS goes here
 
-// var body = document.getElementsByTagName('body')[0];
-// body.style.background = 'linear-gradient(to bottom, #3441FB, #3DC7FC)';
-//
-// var i = 0;
-//  while (i<63) {
-//  var tile  = document.createElement('div');
-//  var randomColor = "#" + Math.floor(Math.random()*16777215).toString(16);
-//   tile.style.width = "11.1%";
-//   tile.style.float = "left"
-//   tile.style.margin = "0";
-//   tile.style.paddingBottom = "11.1%";
-//   tile.style.opacity = "0.5";
-//   if(i % 2 === 0){
-//       tile.style.backgroundColor = "#3DC7FC";
-//     } else {
-//       tile.style.backgroundColor = "#D12DFB";
-//     }
-//     document.body.appendChild(tile);
-//     i++;
-//   }
 
-  var body = document.getElementsByTagName('body')[0];
+var checkerBoard = document.getElementsByTagName('checkerBoard')[0];
+var checkerBoard = document.createElement('div');
+checkerBoard.id = 'checkerBoard';
+document.checkerBoard.appendChild(checkerBoard);
+for(var i = 0; i<63; i++){
+  var tile  = document.createElement('div');
+  var randomColor = "#" + Math.floor(Math.random()*16777215).toString(16);
+  tile.style.width = "11.1%";
+  tile.style.float = "left";
+  tile.style.margin = "0";
+  tile.style.paddingBottom = "11.1%";
+  tile.style.backgroundColor = randomColor;
 
-  var i = 0;
-   while (i<63) {
-   var tile  = document.createElement('div');
-   //var randomColor = "#" + Math.floor(Math.random()*16777215).toString(16);
-  // var flashingColor = setInterval(randomColor, 2000);
-    tile.style.width = "11.1%";
-    tile.style.float = "left";
-    tile.style.margin = "0";
-    tile.style.paddingBottom = "11.1%";
-    tile.style.backgroundColor = function randomColors(){
+  checkerBoard.appendChild(tile);
+}
+
+
+setInterval(function(){
+  var checkerBoard = document.getElementById('checkerBoard');
+  if (checkerBoard.hasChildNodes()) {
+    var children = checkerBoard.childNodes;
+    for (var i = 0; i < children.length; i++) {
       var randomColor = "#" + Math.floor(Math.random()*16777215).toString(16);
-      return randomColor;
-    };
-
-    document.body.appendChild(tile);
-    i++;
+      children[i].style.backgroundColor = randomColor;
+    }
   }
 
-//  setTimeout(randomColors, 2000)
-//setInterval(function() {
-  //  document.body.style.backgroundColor = color(); //() to execute the function!
-//}, 1000);
-//function randomColors(){}
+},2000);
