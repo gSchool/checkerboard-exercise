d9c8075a8bcf313091d74b0a8305dfbad61bb6db
Daniel Stewart <daniel.robbins.stewart@gmail.com>
2016-02-14 15:31:43 -0700

    Minor changes, only first tile will flash still

--- script.js
+++ script.js
@@ -14,10 +14,5 @@ for(i=0; i<9; i++) {
 
 var flashingTiles = setInterval(function() {
  var newTile = document.querySelector('div');
- newTile.style.width = "11.1%";
- newTile.style.paddingBottom = "11.1%";
- newTile.style.float = "left";
  newTile.style.backgroundColor = '#'+Math.floor(Math.random() *16777215).toString(16);
  }, 1000);
-
-flashingTiles();
