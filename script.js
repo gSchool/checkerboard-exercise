53f54876769fa4ad246fa7f7da483e687534a682
Daniel Stewart <daniel.robbins.stewart@gmail.com>
2016-02-14 13:40:05 -0700

    Add code to append first two tiles

--- script.js
+++ script.js
@@ -1,1 +1,21 @@
 // Your JS goes here
+
+var body = document.getElementsByTagName("body")[0];
+var redTile = document.createElement("div");
+var blackTile = document.createElement("div");
+
+
+body.appendChild(redTile);
+body.appendChild(blackTile);
+
+redTile.style.backgroundColor = "red";
+redTile.style.paddingBottom = "11.1%";
+redTile.style.width = "11.1%";
+redTile.style.float = "left";
+redTile.style.position = "relative";
+
+blackTile.style.backgroundColor = "black";
+blackTile.style.paddingBottom = "11.1%";
+blackTile.style.width = "11.1%";
+blackTile.style.float = "left";
+blackTile.style.position = "relative";
