899d0c15f8626bc08222d6d5d128b3a4c3ca65ba
Daniel Stewart <daniel.robbins.stewart@gmail.com>
2016-02-14 15:11:56 -0700

    Add code to make first tile flashing

--- script.js
+++ script.js
@@ -4,7 +4,6 @@ for(i=0; i<9; i++) {
   for(x=0; x<9 ; x++) {
     var newTile = document.createElement('div');
     var body = document.getElementsByTagName('body');
-    newTile.className = "tile";
     document.body.appendChild(newTile);
     newTile.style.width = "11.1%";
     newTile.style.paddingBottom = "11.1%";
@@ -12,3 +11,13 @@ for(i=0; i<9; i++) {
     newTile.style.backgroundColor = '#'+ Math.floor(Math.random() * 16777215).toString(16);
   }
 }
+
+var flashingTiles = setInterval(function() {
+ var newTile = document.querySelector('div');
+ newTile.style.width = "11.1%";
+ newTile.style.paddingBottom = "11.1%";
+ newTile.style.float = "left";
+ newTile.style.backgroundColor = '#'+Math.floor(Math.random() *16777215).toString(16);
+ }, 1000);
+
+flashingTiles();
