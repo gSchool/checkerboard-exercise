711e93605e4a84b451896af40d981ece354fbe66
Daniel Stewart <daniel.robbins.stewart@gmail.com>
2016-02-14 14:24:15 -0700

    Use for loop to create checkered rows

--- script.js
+++ script.js
@@ -1,34 +1,33 @@
 // Your JS goes here
 
-var body = document.getElementsByTagName("body")[0];
-var redTile1 = document.createElement("div");
-var blackTile1 = document.createElement("div");
-var redTile2 = document.createElement("div");
-var blackTile2 = document.createElement("div");
+for(x=0; x<9 ; x++) {
+  var newTile = document.createElement('div');
+  var body = document.getElementsByTagName('body');
+  newTile.className = "tile";
+  document.body.appendChild(newTile);
+  newTile.style.width = "11.1%";
+  newTile.style.paddingBottom = "11.1%";
+  newTile.style.float = "left";
+  if(x % 2 === 0) {
+    newTile.style.backgroundColor = "red";
+  }
+  if(x % 2 === 1) {
+    newTile.style.backgroundColor = "black";
+  }
+}
 
-body.appendChild(redTile1);
-body.appendChild(blackTile1);
-body.appendChild(redTile2);
-body.appendChild(blackTile2);
-
-
-redTile1.style.backgroundColor = "red";
-redTile1.style.paddingBottom = "11.1%";
-redTile1.style.width = "11.1%";
-redTile1.style.float = "left";
-
-
-blackTile1.style.backgroundColor = "black";
-blackTile1.style.paddingBottom = "11.1%";
-blackTile1.style.width = "11.1%";
-blackTile1.style.float = "left";
-
-redTile2.style.backgroundColor = "red";
-redTile2.style.paddingBottom = "11.1%";
-redTile2.style.width = "11.1%";
-redTile2.style.float = "left";
-
-blackTile2.style.backgroundColor = "black";
-blackTile2.style.paddingBottom = "11.1%";
-blackTile2.style.width = "11.1%";
-blackTile2.style.float = "left";
+for(x=0; x<9 ; x++) {
+  var newTile = document.createElement('div');
+  var body = document.getElementsByTagName('body');
+  newTile.className = "tile";
+  document.body.appendChild(newTile);
+  newTile.style.width = "11.1%";
+  newTile.style.paddingBottom = "11.1%";
+  newTile.style.float = "left";
+  if(x % 2 === 0) {
+    newTile.style.backgroundColor = "black";
+  }
+  if(x % 2 === 1) {
+    newTile.style.backgroundColor = "red";
+  }
+}
