ee9a7a9262801e2faa110b2de3e292c2f0a3ddba
Blake Netzeband <blake.netzeband@gmail.com>
2016-02-15 14:16:49 -0700

    completed flashingcolors

--- script.js
+++ script.js
@@ -1,19 +1,21 @@
 // flashingColors
-for (var i = 0; i < 63; i++) {
+setInterval(function() {
   var body = document.querySelector('body');
-  body.style.margin = '0px';
 
-  var randomColor = 'rgb(' + Math.floor(Math.random() * (255)) + ', ' + Math.floor(Math.random() * (255)) + ', ' + Math.floor(Math.random() * (255)) + ')';
-  var changeColorTimer = setInterval(changeColor, 1000);
-  function changeColor () {
+  while ( body.hasChildNodes() )
+  body.removeChild(body.lastChild);
+
+  for (var i = 0; i < 63; i++) {
+    body.style.margin = '0px';
+
+    var randomColor = 'rgb(' + Math.floor(Math.random() * (255)) + ', ' + Math.floor(Math.random() * (255)) + ', ' + Math.floor(Math.random() * (255)) + ')';
+
+    var square = document.createElement('div');
+    square.style.width = '11.1%';
+    square.style.float = 'left';
+    square.style.paddingBottom = '11.1%';
     square.style.backgroundColor = randomColor;
+
+    body.appendChild(square);
   }
-
-  var square = document.createElement('div');
-  square.style.width = '11.1%';
-  square.style.float = 'left';
-  square.style.paddingBottom = '11.1%';
-  square.style.backgroundColor = randomColor;
-
-  body.appendChild(square);
-}
+}, 100);
