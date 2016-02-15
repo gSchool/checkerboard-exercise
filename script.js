549276d26932a3123001adf09d8a83eea85d0564
bud aminof <budaminof@gmail.com>
2016-02-14 19:45:24 -0700

    trying to find a function for grba to change opacity for blue and pink

--- script.js
+++ script.js
@@ -1,7 +1,8 @@
 // Your JS goes here
 
 for (var i = 0; i < 63; i++) {
-  var randomColors = 'rgb(' + (Math.floor(Math.random() * 255)) + ',' + (Math.floor(Math.random() * 255)) + ',' + (Math.floor(Math.random() * 255)) + ')';
+  // var randomColors = 'rgb(' + (Math.floor(Math.random() * 255)) + ',' + (Math.floor(Math.random() * 255)) + ',' + (Math.floor(Math.random() * 255)) + ')';
+
 
   var body = document.querySelector('body');
   body.style.marginLeft= "-5px";
@@ -12,13 +13,13 @@ for (var i = 0; i < 63; i++) {
   black.style.width = "11.1%";
   black.style.float = "left";
   black.style.paddingBottom = "11.1%";
-  black.style.backgroundColor = randomColors;
+  black.style.backgroundColor = "rgba(0, 12, 117, .7)";
 
   var red = document.createElement('div');
   red.style.width = "11.1%";
   red.style.float = "left";
   red.style.paddingBottom = "11.1%";
-  red.style.backgroundColor = randomColors;
+  red.style.backgroundColor = "rgba(0, 12, 117, .3)";
 
   if(i % 2 == 0) {
   body.appendChild(black);
@@ -26,3 +27,12 @@ for (var i = 0; i < 63; i++) {
     body.appendChild(red);
   }
 };
+
+
+var blue = function () {
+  var x= 0.03125;
+  for (var i = x; i < 1; i++){
+    x++;
+  }
+  return "rgba(3, 0, 59," + x + ")"
+}
