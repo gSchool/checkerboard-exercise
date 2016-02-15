dd277e6bcdbb01bcdd1f7c32bf6866c20487af00
Blake Netzeband <blake.netzeband@gmail.com>
2016-02-15 12:51:40 -0700

    created changeColor function, but it isn't working

--- script.js
+++ script.js
@@ -1,9 +1,13 @@
-// randomcolors
+// flashingColors
 for (var i = 0; i < 63; i++) {
   var body = document.querySelector('body');
   body.style.margin = '0px';
 
   var randomColor = 'rgb(' + Math.floor(Math.random() * (255)) + ', ' + Math.floor(Math.random() * (255)) + ', ' + Math.floor(Math.random() * (255)) + ')';
+  var changeColorTimer = setInterval(changeColor, 1000);
+  function changeColor () {
+    square.style.backgroundColor = randomColor;
+  }
 
   var square = document.createElement('div');
   square.style.width = '11.1%';
