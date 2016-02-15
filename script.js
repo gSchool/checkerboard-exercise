30010c6d0d2a7829a34133fb6f9c59cf8b3f92fb
Blake Netzeband <blake.netzeband@gmail.com>
2016-02-15 11:44:59 -0700

    initialized project

--- script.js
+++ script.js
@@ -1,15 +1,22 @@
-// randomcolors
+// gradient
 for (var i = 0; i < 63; i++) {
   var body = document.querySelector('body');
   body.style.margin = '0px';
 
   var randomColor = 'rgb(' + Math.floor(Math.random() * (255)) + ', ' + Math.floor(Math.random() * (255)) + ', ' + Math.floor(Math.random() * (255)) + ')';
 
-  var square = document.createElement('div');
+  var squareEven = document.createElement('div');
   square.style.width = '11.1%';
   square.style.float = 'left';
   square.style.paddingBottom = '11.1%';
   square.style.backgroundColor = randomColor;
 
+  var squareOdd = document.createElement('div');
+  square.style.width = '11.1%';
+  square.style.float = 'left';
+  square.style.paddingBottom = '11.1%';
+  square.style.backgroundColor = randomColor;
+
+  if ()
   body.appendChild(square);
 }
