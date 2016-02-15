62d9f47d8d94a1c1caf3be91f6f690a67bf7caae
Matthew Bouchard <bouchard.matthewj@gmail.com>
2016-02-15 09:02:57 -0700

    True 8*8 chessboard dimensions

--- script.js
+++ script.js
@@ -1,29 +1,56 @@
-function createTiles() {
+function createTilesOddRow() {
 var redTile = document.createElement('div');
 redTile.classname = "odd";
-redTile.style.width = "11.1%";
-redTile.style.paddingBottom = "11.1%";
+redTile.style.width = "12.5%";
+redTile.style.paddingBottom = "12.5%";
 redTile.style.background = 'red';
 redTile.style.float = "left";
 document.body.appendChild(redTile);
 
 var blackTile = document.createElement('div');
-blackTile.classname = "odd";
-blackTile.style.width = "11.1%";
-blackTile.style.paddingBottom = "11.1%";
+blackTile.classname = "even";
+blackTile.style.width = "12.5%";
+blackTile.style.paddingBottom = "12.5%";
 blackTile.style.background = 'black';
 blackTile.style.float = "left";
 document.body.appendChild(blackTile);
 };
 
-for(var i = 1; i <=40; i++){
-  createTiles(i);
+function createTilesEvenRow() {
+var redTile = document.createElement('div');
+redTile.classname = "odd";
+redTile.style.width = "12.5%";
+redTile.style.paddingBottom = "12.5%";
+redTile.style.background = 'black';
+redTile.style.float = "left";
+document.body.appendChild(redTile);
+
+var blackTile = document.createElement('div');
+blackTile.classname = "even";
+blackTile.style.width = "12.5%";
+blackTile.style.paddingBottom = "12.5%";
+blackTile.style.background = 'red';
+blackTile.style.float = "left";
+document.body.appendChild(blackTile);
+};
+
+function createChessBoard(){
+for(var i = 1; i <=4; i++){
+  createTilesOddRow(i);
+  }
+for(var i = 1; i <=4; i++){
+  createTilesEvenRow(i);
+  }
+}
+for(var i = 1; i <= 4; i++){
+  createChessBoard(i);
 }
 
-var finalTile = document.createElement('div');
-finalTile.classname = "odd";
-finalTile.style.width = "11.1%";
-finalTile.style.paddingBottom = "11.1%";
-finalTile.style.background = 'red';
-finalTile.style.float = "left";
-document.body.appendChild(finalTile);
+
+// var finalTile = document.createElement('div');
+// finalTile.classname = "odd";
+// finalTile.style.width = "11.1%";
+// finalTile.style.paddingBottom = "11.1%";
+// finalTile.style.background = 'red';
+// finalTile.style.float = "left";
+// document.body.appendChild(finalTile);
