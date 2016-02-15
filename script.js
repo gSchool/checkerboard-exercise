7ee05ed54fdb79d97a795f8e72be2089454c7b4d
Nathan Reginato <nathan.reginato@gmail.com>
2016-02-15 16:35:51 -0700

    created checkerboard

--- script.js
+++ script.js
@@ -1,1 +1,15 @@
 // Your JS goes here
+
+for (var i = 0; i < 99; i++) {
+  var div = document.createElement("div");
+  document.body.appendChild(div);
+  div.style.float="right";
+  div.style.paddingBottom="11.1%";
+  div.style.width="11.1%";
+  if (i % 2 === 0) {
+    div.style.backgroundColor="red";
+  } else {
+    div.style.backgroundColor="black  ";
+  }
+  console.log(div)
+};
