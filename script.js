7f4421cf7463953006c37fa54df2151b3e376a12
Lincoln Dennis <j.lincoln.dennis@gmail.com>
2016-02-15 15:16:49 -0700

    Included script to generate checkerboard

--- script.js
+++ script.js
@@ -1,1 +1,25 @@
-// Your JS goes here
+for ( var i = 0; i < 81; i++ ) {
+
+  var body = document.getElementsByTagName('body')[0];
+
+  body.style.margin= "0";
+
+  var square = document.createElement('div');
+
+  square.style.width = "11.1%";
+  square.style.float = "left";
+  square.style.paddingBottom = "11.1%";
+
+  if( i % 2 == 0 ) {
+
+    square.style.backgroundColor = 'black';
+
+  } else {
+
+  square.style.backgroundColor = 'red';
+
+}
+
+  body.appendChild(square);
+  
+};
