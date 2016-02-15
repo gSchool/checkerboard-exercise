c7c8d8ae042cd272f81f5f9c29847d9f78bd76ac
bud aminof <budaminof@gmail.com>
2016-02-14 18:50:24 -0700

    board with random colors

--- script.js
+++ script.js
@@ -1,6 +1,8 @@
 // Your JS goes here
 
 for (var i = 0; i < 63; i++) {
+  var randomColors = 'rgb(' + (Math.floor(Math.random() * 255)) + ',' + (Math.floor(Math.random() * 255)) + ',' + (Math.floor(Math.random() * 255)) + ')';
+
   var body = document.querySelector('body');
   body.style.marginLeft= "-5px";
   body.style.marginRight= "-5px";
@@ -10,13 +12,13 @@ for (var i = 0; i < 63; i++) {
   black.style.width = "11.1%";
   black.style.float = "left";
   black.style.paddingBottom = "11.1%";
-  black.style.backgroundColor = "rgb(0, 0, 0)";
+  black.style.backgroundColor = randomColors;
 
   var red = document.createElement('div');
   red.style.width = "11.1%";
   red.style.float = "left";
   red.style.paddingBottom = "11.1%";
-  red.style.backgroundColor = "rgb(200, 35, 35)";
+  red.style.backgroundColor = randomColors;
 
   if(i % 2 == 0) {
   body.appendChild(black);
