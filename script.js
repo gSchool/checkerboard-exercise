8c4d0ec8e524827b18f40d4b2a644807c4513f11
bud aminof <budaminof@gmail.com>
2016-02-15 11:40:05 -0700

    made the code shorter, still works

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 // Your JS goes here
 
 for (var i = 0; i < 63; i++) {
-  
+
   var randomColors = 'rgb(' + (Math.floor(Math.random() * 255)) + ',' + (Math.floor(Math.random() * 255)) + ',' + (Math.floor(Math.random() * 255)) + ')';
 
   var body = document.querySelector('body');
@@ -9,21 +9,11 @@ for (var i = 0; i < 63; i++) {
   body.style.marginRight= "-5px";
   body.style.marginTop= "-5px";
 
-  var black = document.createElement('div');
-  black.style.width = "11.1%";
-  black.style.float = "left";
-  black.style.paddingBottom = "11.1%";
-  black.style.backgroundColor = randomColors;
+  var tile = document.createElement('div');
+  tile.style.width = "11.1%";
+  tile.style.float = "left";
+  tile.style.paddingBottom = "11.1%";
+  tile.style.backgroundColor = randomColors;
 
-  var red = document.createElement('div');
-  red.style.width = "11.1%";
-  red.style.float = "left";
-  red.style.paddingBottom = "11.1%";
-  red.style.backgroundColor = randomColors;
-
-  if(i % 2 == 0) {
-  body.appendChild(black);
-  } else {
-    body.appendChild(red);
-  }
+  body.appendChild(tile);
 };
