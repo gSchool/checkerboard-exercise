f79994d3564b1af8ce3ce1ce129affbd70ed6306
bud aminof <budaminof@gmail.com>
2016-02-15 12:25:29 -0700

    trying a loop for the rgba but its not working yet

--- script.js
+++ script.js
@@ -1,31 +1,33 @@
 // Your JS goes here
-for (var x = 0; x < 7; x++) {
-  
-  for (var i = 0; i < 9; i++) {
+
+  for (var i = 0; i < 63; i++) {
 
     var body = document.querySelector('body');
+
     body.style.marginLeft= "-5px";
     body.style.marginRight= "-5px";
     body.style.marginTop= "-5px";
+
+    var counter = 0;
+    var blueGra = "rgba(1, 7, 54, " + " ." + counter + ")";
+    var pinkGra = "rgb(70, 4, 59, " + " ." + counter + ")";
 
     var blue = document.createElement('div');
     blue.style.width = "11.1%";
     blue.style.float = "left";
     blue.style.paddingBottom = "11.1%";
-    blue.style.backgroundColor = "rgb(1, 7, 54)";
+    blue.style.backgroundColor = blueGra;
 
     var pink = document.createElement('div');
     pink.style.width = "11.1%";
     pink.style.float = "left";
     pink.style.paddingBottom = "11.1%";
-    pink.style.backgroundColor = "rgb(70, 4, 59)";
+    pink.style.backgroundColor = pinkGra;
 
-
-    if(i % 2 == 0) {
-    body.appendChild(blue);
-    } else {
-      body.appendChild(pink);
+    if (i < counter) {
+      counter++;
+      if(i % 2 == 0) body.appendChild(blue);
+      else body.appendChild(pink);
     }
-  }
-
-}
+    
+  };
