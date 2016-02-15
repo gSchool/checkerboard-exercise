84a51ff50dd89f0c8ccb34d21fb588344cb4b478
Blake Netzeband <blake.netzeband@gmail.com>
2016-02-15 12:25:34 -0700

    finsihed 80% of gradient

--- script.js
+++ script.js
@@ -3,20 +3,26 @@ for (var i = 0; i < 63; i++) {
   var body = document.querySelector('body');
   body.style.margin = '0px';
 
-  var randomColor = 'rgb(' + Math.floor(Math.random() * (255)) + ', ' + Math.floor(Math.random() * (255)) + ', ' + Math.floor(Math.random() * (255)) + ')';
+  var counter = 0;
+  var squareEvenGradient = 'rgba (102, 207, 222, ' + '.' + counter + ')';
+  var squareOddGradient = 'rgba (231, 83, 225, ' + '.' + counter + ')';
 
   var squareEven = document.createElement('div');
-  square.style.width = '11.1%';
-  square.style.float = 'left';
-  square.style.paddingBottom = '11.1%';
-  square.style.backgroundColor = randomColor;
+  squareEven.style.width = '11.1%';
+  squareEven.style.float = 'left';
+  squareEven.style.paddingBottom = '11.1%';
+  squareEven.style.backgroundColor = squareEvenGradient;
 
   var squareOdd = document.createElement('div');
-  square.style.width = '11.1%';
-  square.style.float = 'left';
-  square.style.paddingBottom = '11.1%';
-  square.style.backgroundColor = randomColor;
+  squareOdd.style.width = '11.1%';
+  squareOdd.style.float = 'left';
+  squareOdd.style.paddingBottom = '11.1%';
+  squareOdd.style.backgroundColor = squareOddGradient;
 
-  if ()
-  body.appendChild(square);
+  if ( i < counter ) {
+    counter++;
+    if ( i % 2 === 0) {
+      body.appendChild(squareEven);
+    } else {body.appendChild(squareOdd)};
+  }
 }
