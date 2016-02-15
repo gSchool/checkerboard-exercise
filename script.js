2244a1c28f75e04d8e025fa04d297f3869116e3e
Matthew Bouchard <bouchard.matthewj@gmail.com>
2016-02-14 20:54:07 -0700

    one row of the checkerboard

--- script.js
+++ script.js
@@ -1,1 +1,285 @@
-// Your JS goes here
+var tile1 = document.createElement('div');
+
+tile1.style.width= "11.1%";
+tile1.style.paddingBottom = "11.1%";
+tile1.style.background = 'red';
+tile1.style.float = "left";
+document.body.appendChild(tile1);
+
+var tile2 = document.createElement('div');
+
+tile2.style.width= "11.1%";
+tile2.style.paddingBottom = "11.1%";
+tile2.style.background = 'black';
+tile2.style.float = "left";
+document.body.appendChild(tile2);
+
+var tile3 = document.createElement('div');
+
+tile3.style.width= "11.1%";
+tile3.style.paddingBottom = "11.1%";
+tile3.style.background = 'red';
+tile3.style.float = "left";
+document.body.appendChild(tile3);
+
+var tile4 = document.createElement('div');
+
+tile4.style.width= "11.1%";
+tile4.style.paddingBottom = "11.1%";
+tile4.style.background = 'black';
+tile4.style.float = "left";
+document.body.appendChild(tile4);
+
+var tile5 = document.createElement('div');
+
+tile5.style.width= "11.1%";
+tile5.style.paddingBottom = "11.1%";
+tile5.style.background = 'red';
+tile5.style.float = "left";
+document.body.appendChild(tile5);
+
+var tile6 = document.createElement('div');
+
+tile6.style.width= "11.1%";
+tile6.style.paddingBottom = "11.1%";
+tile6.style.background = 'black';
+tile6.style.float = "left";
+document.body.appendChild(tile6);
+
+var tile7 = document.createElement('div');
+
+tile7.style.width= "11.1%";
+tile7.style.paddingBottom = "11.1%";
+tile7.style.background = 'red';
+tile7.style.float = "left";
+document.body.appendChild(tile7);
+
+var tile8 = document.createElement('div');
+
+tile8.style.width= "11.1%";
+tile8.style.paddingBottom = "11.1%";
+tile8.style.background = 'black';
+tile8.style.float = "left";
+document.body.appendChild(tile8);
+
+var break1 = document.createElement('/br');
+document.body.appendChild(break1);
+
+
+var tile9 = document.createElement('div');
+
+tile9.style.width= "11.1%";
+tile9.style.paddingBottom = "11.1%";
+tile9.style.background = 'red';
+tile9.style.float = "left";
+document.body.appendChild(tile9);
+
+// var tile2 = document.createElement('div');
+//
+// tile2.style.width= "11.1%";
+// // tile2.style.height = "11.1%";
+// tile2.style.paddingBottom = "11.1%";
+// tile2.style.background = 'black';
+// tile2.float = "left";
+// document.body.appendChild(tile2);
+//
+// var tile3 = document.createElement('div');
+//
+// tile3.style.width= "11.1%";
+// // tile3.style.height = "11.1%";
+// tile3.style.paddingBottom = "11.1%";
+// tile3.style.background = 'red';
+// tile3.float = "left";
+// document.body.appendChild(tile3);
+//
+// var tile4 = document.createElement('div');
+//
+// tile4.style.width= "11.1%";
+// // tile4.style.height = "11.1%";
+// tile4.style.paddingBottom = "11.1%";
+// tile4.style.background = 'black';
+// tile4.float = "left";
+// document.body.appendChild(tile4);
+//
+// var tile5 = document.createElement('div');
+//
+// tile5.style.width= "11.1%";
+// // tile5.style.height = "11.1%";
+// tile5.style.paddingBottom = "11.1%";
+// tile5.style.background = 'red';
+// tile5.float = "left";
+// document.body.appendChild(tile5);
+//
+// var tile6 = document.createElement('div');
+//
+// tile6.style.width= "11.1%";
+// // tile6.style.height = "11.1%";
+// tile6.style.paddingBottom = "11.1%";
+// tile6.style.background = 'black';
+// tile6.float = "left";
+// document.body.appendChild(tile6);
+//
+// var tile7 = document.createElement('div');
+//
+// tile7.style.width= "11.1%";
+// // tile7.style.height = "11.1%";
+// tile7.style.paddingBottom = "11.1%";
+// tile7.style.background = 'red';
+// tile7.float = "left";
+// document.body.appendChild(tile7);
+//
+// var tile8 = document.createElement('div');
+//
+// tile8.style.width= "11.1%";
+// // tile8.style.height = "11.1%";
+// tile8.style.paddingBottom = "11.1%";
+// tile8.style.background = 'black';
+// tile8.float = "left";
+// document.body.appendChild(tile8);
+//
+// var tile1 = document.createElement('div');
+//
+// tile1.style.width= "11.1%";
+// // tile1.style.height = "11.1%";
+// tile1.style.paddingBottom = "11.1%";
+// tile1.style.background = 'red';
+// tile1.float = "left";
+// document.body.appendChild(tile1);
+//
+// var tile2 = document.createElement('div');
+//
+// tile2.style.width= "11.1%";
+// // tile2.style.height = "11.1%";
+// tile2.style.paddingBottom = "11.1%";
+// tile2.style.background = 'black';
+// tile2.float = "left";
+// document.body.appendChild(tile2);
+//
+// var tile3 = document.createElement('div');
+//
+// tile3.style.width= "11.1%";
+// // tile3.style.height = "11.1%";
+// tile3.style.paddingBottom = "11.1%";
+// tile3.style.background = 'red';
+// tile3.float = "left";
+// document.body.appendChild(tile3);
+//
+// var tile4 = document.createElement('div');
+//
+// tile4.style.width= "11.1%";
+// // tile4.style.height = "11.1%";
+// tile4.style.paddingBottom = "11.1%";
+// tile4.style.background = 'black';
+// tile4.float = "left";
+// document.body.appendChild(tile4);
+//
+// var tile5 = document.createElement('div');
+//
+// tile5.style.width= "11.1%";
+// // tile5.style.height = "11.1%";
+// tile5.style.paddingBottom = "11.1%";
+// tile5.style.background = 'red';
+// tile5.float = "left";
+// document.body.appendChild(tile5);
+//
+// var tile6 = document.createElement('div');
+//
+// tile6.style.width= "11.1%";
+// // tile6.style.height = "11.1%";
+// tile6.style.paddingBottom = "11.1%";
+// tile6.style.background = 'black';
+// tile6.float = "left";
+// document.body.appendChild(tile6);
+//
+// var tile7 = document.createElement('div');
+//
+// tile7.style.width= "11.1%";
+// // tile7.style.height = "11.1%";
+// tile7.style.paddingBottom = "11.1%";
+// tile7.style.background = 'red';
+// tile7.float = "left";
+// document.body.appendChild(tile7);
+//
+// var tile8 = document.createElement('div');
+//
+// tile8.style.width= "11.1%";
+// // tile8.style.height = "11.1%";
+// tile8.style.paddingBottom = "11.1%";
+// tile8.style.background = 'black';
+// tile8.float = "left";
+// document.body.appendChild(tile8);
+//
+// // var break = document.createElement('br');
+// // document.body.appendChild(break);
+//
+// var tile1 = document.createElement('div');
+//
+// tile1.style.width= "11.1%";
+// // tile1.style.height = "11.1%";
+// tile1.style.paddingBottom = "11.1%";
+// tile1.style.background = 'red';
+// tile1.float = "left";
+// document.body.appendChild(tile1);
+//
+// var tile2 = document.createElement('div');
+//
+// tile2.style.width= "11.1%";
+// // tile2.style.height = "11.1%";
+// tile2.style.paddingBottom = "11.1%";
+// tile2.style.background = 'black';
+// tile2.float = "left";
+// document.body.appendChild(tile2);
+//
+// var tile3 = document.createElement('div');
+//
+// tile3.style.width= "11.1%";
+// // tile3.style.height = "11.1%";
+// tile3.style.paddingBottom = "11.1%";
+// tile3.style.background = 'red';
+// tile3.float = "left";
+// document.body.appendChild(tile3);
+//
+// var tile4 = document.createElement('div');
+//
+// tile4.style.width= "11.1%";
+// // tile4.style.height = "11.1%";
+// tile4.style.paddingBottom = "11.1%";
+// tile4.style.background = 'black';
+// tile4.float = "left";
+// document.body.appendChild(tile4);
+//
+// var tile5 = document.createElement('div');
+//
+// tile5.style.width= "11.1%";
+// // tile5.style.height = "11.1%";
+// tile5.style.paddingBottom = "11.1%";
+// tile5.style.background = 'red';
+// tile5.float = "left";
+// document.body.appendChild(tile5);
+//
+// var tile6 = document.createElement('div');
+//
+// tile6.style.width= "11.1%";
+// // tile6.style.height = "11.1%";
+// tile6.style.paddingBottom = "11.1%";
+// tile6.style.background = 'black';
+// tile6.float = "left";
+// document.body.appendChild(tile6);
+//
+// var tile7 = document.createElement('div');
+//
+// tile7.style.width= "11.1%";
+// // tile7.style.height = "11.1%";
+// tile7.style.paddingBottom = "11.1%";
+// tile7.style.background = 'red';
+// tile7.float = "left";
+// document.body.appendChild(tile7);
+//
+// var tile8 = document.createElement('div');
+//
+// tile8.style.width= "11.1%";
+// // tile8.style.height = "11.1%";
+// tile8.style.paddingBottom = "11.1%";
+// tile8.style.background = 'black';
+// tile8.float = "left";
+// document.body.appendChild(tile8);
