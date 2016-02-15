c83182964c24c1025524fb952100b72b4f3088d6
Madeline Pickering <madelinepickering@gmail.com>
2016-02-15 13:59:08 -0700

    random color checkerboard

--- script.js
+++ script.js
@@ -13,28 +13,14 @@ newDiv.style.width = "11.1%";
 newDiv.style.paddingBottom = "11.1%";
 newDiv.style.float = "left";
 
-if (i%2 === 0) {
-  newDiv.style.backgroundColor = "black";
+
+function getRandomColor() {
+    var letters = '0123456789ABCDEF'.split('');
+    var color = '#';
+    for (var i = 0; i < 6; i++ ) {
+        color += letters[Math.floor(Math.random() * 16)];
+    }
+    return color;
   }
-else {
-  newDiv.style.backgroundColor = "red";
-  }
+newDiv.style.backgroundColor = getRandomColor();
 }
-
-
-
-
-//
-// var newImg = document.createElement("img");
-// newImg.src = "http://placekitten.com/345/543";
-// var newH1 = document.createElement("h1");
-// var newP = document.createElement("p");
-//
-// var body = document.getElementsByTagName("body")[0];
-// body.appendChild(newImg);
-// body.appendChild(newH1);
-// body.appendChild(newP);
-//
-// newH1.innerText = "Cats have invaded";
-// newImg.src = "http://placekitten.com/444/567";
-// newP.innerText = "We're here to take over!";
