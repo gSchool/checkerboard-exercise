4971625146bbd20ad53885a43b9942637fc4ddb8
bud aminof <budaminof@gmail.com>
2016-02-15 14:31:44 -0700

    setting background Image to gradient for the body- did the trick!

--- script.js
+++ script.js
@@ -7,27 +7,22 @@
     body.style.marginLeft= "-5px";
     body.style.marginRight= "-5px";
     body.style.marginTop= "-5px";
+    body.style.backgroundImage = "linear-gradient(rgb(36, 23, 82), rgb(248, 250, 174))";
 
-    var counter = 0;
-    var blueGra = "rgba(1, 7, 54, " + " ." + counter + ")";
-    var pinkGra = "rgb(70, 4, 59, " + " ." + counter + ")";
 
     var blue = document.createElement('div');
     blue.style.width = "11.1%";
     blue.style.float = "left";
     blue.style.paddingBottom = "11.1%";
-    blue.style.backgroundColor = blueGra;
+    blue.style.backgroundColor = "rgba(6, 4, 70, 0.4)";
 
     var pink = document.createElement('div');
     pink.style.width = "11.1%";
     pink.style.float = "left";
     pink.style.paddingBottom = "11.1%";
-    pink.style.backgroundColor = pinkGra;
+    pink.style.backgroundColor = "rgba(93, 3, 8, 0.4)";
 
-    if (i < counter) {
-      counter++;
-      if(i % 2 == 0) body.appendChild(blue);
-      else body.appendChild(pink);
-    }
-    
+  if( i % 2 == 0) body.appendChild(blue);
+  else body.appendChild(pink);
+
   };
