8a87cfa01f63c11f58394624faa6c3badbf5ed97
jason goldfarb <jasongoldfarb@jasons-MacBook-Pro.local>
2016-02-15 13:51:59 -0700

    checkboard does not work boo

--- script.js
+++ script.js
@@ -1,1 +1,29 @@
-// Your JS goes here
+
+
+for (var i = 0; i < 5000; i++) {
+var body =
+document.querySelector("body");
+  body.style.marginBottom = "-4px";
+  body.style.marginLeft = "-4px";
+  body.style.marginRight = "-4px";
+  body.style.marginTop = "-4px";
+
+var redBox =
+document.createElement("div");
+  redBox.style.width = '11.1%';
+  redBox.style.float = 'left';
+  redBox.style.paddingBottom ='11.1%';
+  redBox.style.backgroundColor = "red";
+
+  var blackBox =
+  document.createElement("div");
+    blackBox.style.width = '11.1%';
+    blackBox.style.float ='left';
+    blackBox.style.paddingBottom ='11.1%';
+    blackBox.style.backgroundColor = 'black';
+  }
+  if (i % 2 == 0) {
+     body.appendChild(blackBox);
+  } else {
+     body.appendChild(redBox);
+  }
