1d7d4e5d97db38d269fd68eb2cdf3fd30445f392
bud aminof <budaminof@gmail.com>
2016-02-15 11:07:04 -0700

    two functions for changing colors

--- script.js
+++ script.js
@@ -1,9 +1,7 @@
 // Your JS goes here
 
 for (var i = 0; i < 63; i++) {
-
-  // var randomColors = 'rgb(' + (Math.floor(Math.random() * 255)) + ',' + (Math.floor(Math.random() * 255)) + ',' + (Math.floor(Math.random() * 255)) + ')';
-
+  
   var intervalBlue = setInterval(function () {
   var randomColors = 'rgb(' + (Math.floor(Math.random() * 255)) + ',' + (Math.floor(Math.random() * 255)) + ',' + (Math.floor(Math.random() * 255)) + ')';
   return blue.style.backgroundColor = randomColors;
