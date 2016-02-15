34bd5d69e631f014c326db8b28332435662221de
Harrison Fischberg <hrfischberg@gmail.com>
2016-02-14 22:53:03 -0700

    random colors with question

--- script.js
+++ script.js
@@ -1,21 +1,21 @@
-// var squares = document.createElement('div');
-// document.body.appendChild(squares);
+var squares = document.createElement('div');
+document.body.appendChild(squares);
+var randomColors = ["#61FF4C","#00FF00","#84007C","#B9AF12","#1500F5","#58E869","#F30053","#FB0006"]
 
-// for(i=0; i<= 100; i++){
-//   if(i < 99){
-//     var square = document.body.appendChild(document.createElement("div"))
-//     square.style.paddingBottom = "11.1%";
-//     square.style.float = "left";
-//     square.style.width = "11.1%";
+for(i=0; i<= 100; i++){
+  var randomColor = randomColors[Math.floor(Math.random() * 8)];
+  var square = document.body.appendChild(document.createElement("div"))
+  square.style.paddingBottom = "11.1%";
+  square.style.float = "left";
+  square.style.width = "11.1%";
+  square.style.backgroundColor = randomColor;
+  document.body.appendChild(squares);
+}
 //     if(i % 2 === 0){
 //       square.style.backgroundColor = "black"
 //     } else{
 //       square.style.backgroundColor = "red"
 //     }
-    
-    // document.body.appendChild(squares);
-//   }
-// }
 
 
-
+// ASK IN CLASS -- - - - - -   "#"+((1<<24)*Math.random()|0).toString(16);
