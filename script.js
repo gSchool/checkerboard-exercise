a9705f29c77b270acb29d79044f296d7b14edd5c
Madeline Pickering <madelinepickering@gmail.com>
2016-02-15 13:48:04 -0700

    black and red checkerboard

--- script.js
+++ script.js
@@ -1,1 +1,40 @@
-// Your JS goes here
+var body  = document.querySelector('body');
+for (var i = 0; i < 2000; i++) {
+var newDiv = document.createElement('div');
+
+body.style.marginLeft = "-4px";
+body.style.marginRight = "-4px";
+body.style.marginTop = "-4px";
+body.style.marginBottom = "-4px";
+
+body.appendChild(newDiv);
+
+newDiv.style.width = "11.1%";
+newDiv.style.paddingBottom = "11.1%";
+newDiv.style.float = "left";
+
+if (i%2 === 0) {
+  newDiv.style.backgroundColor = "black";
+  }
+else {
+  newDiv.style.backgroundColor = "red";
+  }
+}
+
+
+
+
+//
+// var newImg = document.createElement("img");
+// newImg.src = "http://placekitten.com/345/543";
+// var newH1 = document.createElement("h1");
+// var newP = document.createElement("p");
+//
+// var body = document.getElementsByTagName("body")[0];
+// body.appendChild(newImg);
+// body.appendChild(newH1);
+// body.appendChild(newP);
+//
+// newH1.innerText = "Cats have invaded";
+// newImg.src = "http://placekitten.com/444/567";
+// newP.innerText = "We're here to take over!";
