8ff934379a23358aab77c96e3947500870557c94
Wfuger <wfuger@gmail.com>
2016-02-15 11:00:39 -0700

    Checkerboard red and black

--- script.js
+++ script.js
@@ -1,1 +1,380 @@
 // Your JS goes here
+var body = document.getElementsByTagName('body')[0];
+
+var newDiv = document.createElement('div');
+
+newDiv.style.width = '11.1%';
+newDiv.style.cssFloat = 'left';
+newDiv.style.paddingBottom = '11.1%';
+newDiv.style.backgroundColor = 'black';
+
+body.appendChild(newDiv);
+
+
+var newDiv = document.createElement('div');
+
+
+newDiv.style.width = '11.1%';
+newDiv.style.cssFloat = 'left';
+newDiv.style.paddingBottom = '11.1%';
+newDiv.style.backgroundColor = 'red';
+
+
+body.appendChild(newDiv);
+var newDiv = document.createElement('div');
+
+
+newDiv.style.width = '11.1%';
+newDiv.style.cssFloat = 'left';
+newDiv.style.paddingBottom = '11.1%';
+newDiv.style.backgroundColor = 'black';
+
+
+body.appendChild(newDiv);
+
+var newDiv = document.createElement('div');
+
+
+newDiv.style.width = '11.1%';
+newDiv.style.cssFloat = 'left';
+newDiv.style.paddingBottom = '11.1%';
+newDiv.style.backgroundColor = 'red';
+
+
+body.appendChild(newDiv);
+var newDiv = document.createElement('div');
+
+
+newDiv.style.width = '11.1%';
+newDiv.style.cssFloat = 'left';
+newDiv.style.paddingBottom = '11.1%';
+newDiv.style.backgroundColor = 'black';
+
+
+body.appendChild(newDiv);
+
+var newDiv = document.createElement('div');
+
+
+newDiv.style.width = '11.1%';
+newDiv.style.cssFloat = 'left';
+newDiv.style.paddingBottom = '11.1%';
+newDiv.style.backgroundColor = 'red';
+
+
+body.appendChild(newDiv);
+var newDiv = document.createElement('div');
+
+
+newDiv.style.width = '11.1%';
+newDiv.style.cssFloat = 'left';
+newDiv.style.paddingBottom = '11.1%';
+newDiv.style.backgroundColor = 'black';
+
+
+body.appendChild(newDiv);
+
+var newDiv = document.createElement('div');
+
+
+newDiv.style.width = '11.1%';
+newDiv.style.cssFloat = 'left';
+newDiv.style.paddingBottom = '11.1%';
+newDiv.style.backgroundColor = 'red';
+
+
+body.appendChild(newDiv);
+var newDiv = document.createElement('div');
+
+
+newDiv.style.width = '11.1%';
+newDiv.style.cssFloat = 'left';
+newDiv.style.paddingBottom = '11.1%';
+newDiv.style.backgroundColor = 'black';
+
+
+body.appendChild(newDiv);
+
+var newDiv = document.createElement('div');
+
+
+newDiv.style.width = '11.1%';
+newDiv.style.cssFloat = 'left';
+newDiv.style.paddingBottom = '11.1%';
+newDiv.style.backgroundColor = 'red';
+
+
+body.appendChild(newDiv);
+var newDiv = document.createElement('div');
+
+
+newDiv.style.width = '11.1%';
+newDiv.style.cssFloat = 'left';
+newDiv.style.paddingBottom = '11.1%';
+newDiv.style.backgroundColor = 'black';
+
+
+body.appendChild(newDiv);
+
+var newDiv = document.createElement('div');
+
+
+newDiv.style.width = '11.1%';
+newDiv.style.cssFloat = 'left';
+newDiv.style.paddingBottom = '11.1%';
+newDiv.style.backgroundColor = 'red';
+
+
+body.appendChild(newDiv);
+var newDiv = document.createElement('div');
+
+
+newDiv.style.width = '11.1%';
+newDiv.style.cssFloat = 'left';
+newDiv.style.paddingBottom = '11.1%';
+newDiv.style.backgroundColor = 'black';
+
+
+body.appendChild(newDiv);
+
+var newDiv = document.createElement('div');
+
+
+newDiv.style.width = '11.1%';
+newDiv.style.cssFloat = 'left';
+newDiv.style.paddingBottom = '11.1%';
+newDiv.style.backgroundColor = 'red';
+
+
+body.appendChild(newDiv);
+var newDiv = document.createElement('div');
+
+
+newDiv.style.width = '11.1%';
+newDiv.style.cssFloat = 'left';
+newDiv.style.paddingBottom = '11.1%';
+newDiv.style.backgroundColor = 'black';
+
+
+body.appendChild(newDiv);
+
+var newDiv = document.createElement('div');
+
+
+newDiv.style.width = '11.1%';
+newDiv.style.cssFloat = 'left';
+newDiv.style.paddingBottom = '11.1%';
+newDiv.style.backgroundColor = 'red';
+
+
+body.appendChild(newDiv);
+var newDiv = document.createElement('div');
+
+
+newDiv.style.width = '11.1%';
+newDiv.style.cssFloat = 'left';
+newDiv.style.paddingBottom = '11.1%';
+newDiv.style.backgroundColor = 'black';
+
+
+body.appendChild(newDiv);
+
+var newDiv = document.createElement('div');
+
+
+newDiv.style.width = '11.1%';
+newDiv.style.cssFloat = 'left';
+newDiv.style.paddingBottom = '11.1%';
+newDiv.style.backgroundColor = 'red';
+
+
+body.appendChild(newDiv);
+var newDiv = document.createElement('div');
+
+
+newDiv.style.width = '11.1%';
+newDiv.style.cssFloat = 'left';
+newDiv.style.paddingBottom = '11.1%';
+newDiv.style.backgroundColor = 'black';
+
+
+body.appendChild(newDiv);
+
+var newDiv = document.createElement('div');
+
+
+newDiv.style.width = '11.1%';
+newDiv.style.cssFloat = 'left';
+newDiv.style.paddingBottom = '11.1%';
+newDiv.style.backgroundColor = 'red';
+
+
+body.appendChild(newDiv);
+var newDiv = document.createElement('div');
+
+
+newDiv.style.width = '11.1%';
+newDiv.style.cssFloat = 'left';
+newDiv.style.paddingBottom = '11.1%';
+newDiv.style.backgroundColor = 'black';
+
+
+body.appendChild(newDiv);
+
+var newDiv = document.createElement('div');
+
+
+newDiv.style.width = '11.1%';
+newDiv.style.cssFloat = 'left';
+newDiv.style.paddingBottom = '11.1%';
+newDiv.style.backgroundColor = 'red';
+
+
+body.appendChild(newDiv);
+var newDiv = document.createElement('div');
+
+
+newDiv.style.width = '11.1%';
+newDiv.style.cssFloat = 'left';
+newDiv.style.paddingBottom = '11.1%';
+newDiv.style.backgroundColor = 'black';
+
+
+body.appendChild(newDiv);
+
+var newDiv = document.createElement('div');
+
+
+newDiv.style.width = '11.1%';
+newDiv.style.cssFloat = 'left';
+newDiv.style.paddingBottom = '11.1%';
+newDiv.style.backgroundColor = 'red';
+
+
+body.appendChild(newDiv);
+var newDiv = document.createElement('div');
+
+
+newDiv.style.width = '11.1%';
+newDiv.style.cssFloat = 'left';
+newDiv.style.paddingBottom = '11.1%';
+newDiv.style.backgroundColor = 'black';
+
+
+body.appendChild(newDiv);
+
+var newDiv = document.createElement('div');
+
+
+newDiv.style.width = '11.1%';
+newDiv.style.cssFloat = 'left';
+newDiv.style.paddingBottom = '11.1%';
+newDiv.style.backgroundColor = 'red';
+
+
+body.appendChild(newDiv);
+var newDiv = document.createElement('div');
+
+
+newDiv.style.width = '11.1%';
+newDiv.style.cssFloat = 'left';
+newDiv.style.paddingBottom = '11.1%';
+newDiv.style.backgroundColor = 'black';
+
+
+body.appendChild(newDiv);
+
+var newDiv = document.createElement('div');
+
+
+newDiv.style.width = '11.1%';
+newDiv.style.cssFloat = 'left';
+newDiv.style.paddingBottom = '11.1%';
+newDiv.style.backgroundColor = 'red';
+
+
+body.appendChild(newDiv);
+var newDiv = document.createElement('div');
+
+
+newDiv.style.width = '11.1%';
+newDiv.style.cssFloat = 'left';
+newDiv.style.paddingBottom = '11.1%';
+newDiv.style.backgroundColor = 'black';
+
+
+body.appendChild(newDiv);
+
+var newDiv = document.createElement('div');
+
+
+newDiv.style.width = '11.1%';
+newDiv.style.cssFloat = 'left';
+newDiv.style.paddingBottom = '11.1%';
+newDiv.style.backgroundColor = 'red';
+
+
+body.appendChild(newDiv);
+var newDiv = document.createElement('div');
+
+
+newDiv.style.width = '11.1%';
+newDiv.style.cssFloat = 'left';
+newDiv.style.paddingBottom = '11.1%';
+newDiv.style.backgroundColor = 'black';
+
+
+body.appendChild(newDiv);
+
+var newDiv = document.createElement('div');
+
+
+newDiv.style.width = '11.1%';
+newDiv.style.cssFloat = 'left';
+newDiv.style.paddingBottom = '11.1%';
+newDiv.style.backgroundColor = 'red';
+
+
+body.appendChild(newDiv);
+var newDiv = document.createElement('div');
+
+
+newDiv.style.width = '11.1%';
+newDiv.style.cssFloat = 'left';
+newDiv.style.paddingBottom = '11.1%';
+newDiv.style.backgroundColor = 'black';
+
+
+body.appendChild(newDiv);
+
+var newDiv = document.createElement('div');
+
+
+newDiv.style.width = '11.1%';
+newDiv.style.cssFloat = 'left';
+newDiv.style.paddingBottom = '11.1%';
+newDiv.style.backgroundColor = 'red';
+
+
+body.appendChild(newDiv);
+var newDiv = document.createElement('div');
+
+
+newDiv.style.width = '11.1%';
+newDiv.style.cssFloat = 'left';
+newDiv.style.paddingBottom = '11.1%';
+newDiv.style.backgroundColor = 'black';
+
+
+body.appendChild(newDiv);
+
+var newDiv = document.createElement('div');
+
+
+newDiv.style.width = '11.1%';
+newDiv.style.cssFloat = 'left';
+newDiv.style.paddingBottom = '11.1%';
+newDiv.style.backgroundColor = 'red';
+
+
+body.appendChild(newDiv);
