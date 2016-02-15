f323dfff46e641d08b74573d45d7e6b62234be05
Phil Skaggs <prskaggsjr@gmail.com>
2016-02-15 16:25:05 -0700

    Random colors work

--- script.js
+++ script.js
@@ -8,18 +8,20 @@ for( var i = 0; i < 3000; i++ ) {
 
   divOdd.style.width = '11.1%';
   divOdd.style.paddingBottom = '11.1%';
-  divOdd.style.backgroundColor = 'red';
+  divOdd.style.backgroundColor = 'rgb(' + (Math.floor(Math.random() * 256)) + ',' + (Math.floor(Math.random() * 256)) + ',' + (Math.floor(Math.random() * 256)) + ')';
   divOdd.style.float = 'left';
 
   var divEven= document.createElement('div');
 
   divEven.style.width = '11.1%';
   divEven.style.paddingBottom = '11.1%';
-  divEven.style.backgroundColor = 'black';
+  divEven.style.backgroundColor = 'rgb(' + (Math.floor(Math.random() * 256)) + ',' + (Math.floor(Math.random() * 256)) + ',' + (Math.floor(Math.random() * 256)) + ')';
   divEven.style.float = 'left';
 
   if(i % 2 !== 0){ document.body.appendChild(divOdd); }
 
   else { document.body.appendChild(divEven); }
+
+
 
 }
