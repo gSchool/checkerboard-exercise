6a2df93776d47c69218c44c89520e631590c59bf
Madeline Pickering <madelinepickering@gmail.com>
2016-02-15 14:23:22 -0700

    gradient added

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
+
+for (var i = 0; i < 63; i++) {
 var body  = document.querySelector('body');
-for (var i = 0; i < 2000; i++) {
 var newDiv = document.createElement('div');
 
 body.style.marginLeft = "-4px";
@@ -13,35 +14,14 @@ newDiv.style.width = "11.1%";
 newDiv.style.paddingBottom = "11.1%";
 newDiv.style.float = "left";
 
+body.style.backgroundImage = "linear-gradient(blue, orange)";
+
 if (i%2 === 0) {
   newDiv.style.backgroundColor = "blue";
+  newDiv.style.opacity = ".4";
   }
 else {
   newDiv.style.backgroundColor = "green";
-  }
-  
-if (i>=9) {
-  newDiv.style.opacity = ".1";
-  }
-else {
-  newDiv.style.backgroundColor = "green";
+  newDiv.style.opacity = ".4";
   }
 }
-
-
-
-
-//
-// var newImg = document.createElement("img");
-// newImg.src = "http://placekitten.com/345/543";
-// var newH1 = document.createElement("h1");
-// var newP = document.createElement("p");
-//
-// var body = document.getElementsByTagName("body")[0];
-// body.appendChild(newImg);
-// body.appendChild(newH1);
-// body.appendChild(newP);
-//
-// newH1.innerText = "Cats have invaded";
-// newImg.src = "http://placekitten.com/444/567";
-// newP.innerText = "We're here to take over!";
