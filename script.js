facb52c9dacb0f708d916570b6b80ce44a21f137
Blake Netzeband <blake.netzeband@gmail.com>
2016-02-15 11:38:53 -0700

    created script.js file'

--- script.js
+++ script.js
@@ -3,19 +3,11 @@ for (var i = 0; i < 63; i++) {
   var body = document.querySelector('body');
   body.style.margin = '0px';
 
-  var black = document.createElement('div');
-  black.style.width = '11.1%';
-  black.style.float = 'left';
-  black.style.paddingBottom = '11.1%';
-  black.style.backgroundColor = 'black';
+  var randomColor = 'rgb(' + Math.floor(Math.random() * (255)) + ', ' + Math.floor(Math.random() * (255)) + ', ' + Math.floor(Math.random() * (255)) + ')';
 
-  var red = document.createElement('div');
-  red.style.width = '11.1%';
-  red.style.float = 'left';
-  red.style.paddingBottom = '11.1%';
-  red.style.backgroundColor = 'red';
-
-  if ( i % 2 === 0 ) {
-    body.appendChild(black);
-  } else { body.appendChild(red)}
+  var square = document.createElement('div');
+  square.style.width = '11.1%';
+  square.style.float = 'left';
+  square.style.paddingBottom = '11.1%';
+  square.style.backgroundColor = 'blue';
 }
