a7dfbaa9d0ca52819e0111987390a8d2019b841e
bud aminof <budaminof@gmail.com>
2016-02-15 12:07:12 -0700

    trying

--- script.js
+++ script.js
@@ -1,29 +1,31 @@
 // Your JS goes here
+for (var x = 0; x < 7; x++) {
+  
+  for (var i = 0; i < 9; i++) {
 
-for (var i = 0; i < 63; i++) {
+    var body = document.querySelector('body');
+    body.style.marginLeft= "-5px";
+    body.style.marginRight= "-5px";
+    body.style.marginTop= "-5px";
 
-  var body = document.querySelector('body');
-  body.style.marginLeft= "-5px";
-  body.style.marginRight= "-5px";
-  body.style.marginTop= "-5px";
+    var blue = document.createElement('div');
+    blue.style.width = "11.1%";
+    blue.style.float = "left";
+    blue.style.paddingBottom = "11.1%";
+    blue.style.backgroundColor = "rgb(1, 7, 54)";
 
-  var blue = document.createElement('div');
-  blue.style.width = "11.1%";
-  blue.style.float = "left";
-  blue.style.paddingBottom = "11.1%";
-  blue.style.backgroundColor = "rgb(1, 7, 54)";
+    var pink = document.createElement('div');
+    pink.style.width = "11.1%";
+    pink.style.float = "left";
+    pink.style.paddingBottom = "11.1%";
+    pink.style.backgroundColor = "rgb(70, 4, 59)";
 
-  var pink = document.createElement('div');
-  pink.style.width = "11.1%";
-  pink.style.float = "left";
-  pink.style.paddingBottom = "11.1%";
-  pink.style.backgroundColor = "rgb(70, 4, 59)";
 
-  //find every line. and give it rgba starting from 1 down by floating point.
-
-  if(i % 2 == 0) {
-  body.appendChild(blue);
-  } else {
-    body.appendChild(pink);
+    if(i % 2 == 0) {
+    body.appendChild(blue);
+    } else {
+      body.appendChild(pink);
+    }
   }
-};
+
+}
