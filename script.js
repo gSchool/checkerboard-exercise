5070175128489aa54664d3b1f24cd58fd1387c1f
Lincoln Dennis <j.lincoln.dennis@gmail.com>
2016-02-15 17:05:35 -0700

    Add gradient

--- script.js
+++ script.js
@@ -1,17 +1,20 @@
 for ( var i = 0; i < 81; i++ ) {
 
-  var body = document.getElementsByTagName('body')[0];
+ var body = document.getElementsByTagName('body')[0];
 
   body.style.margin= "0";
 
   var square = document.createElement('div');
-  var randoColor = 'rgba(' + (Math.floor(Math.random() * 255)) + ',' + (Math.floor(Math.random() * 255)) + ',' + (Math.floor(Math.random() * 255)) + ' ,' + (Math.random()) + ')';
 
   square.style.width = "11.1%";
   square.style.float = "left";
   square.style.paddingBottom = "11.1%";
-  square.style.backgroundColor = randoColor;
+  body.style.background = 'linear-gradient(to bottom right, red, green)';
 
+  if( i % 2 == 0 ) {
+    square.style.backgroundColor = '';
+  } else {
+    square.style.background = 'blue';
+  }
   body.appendChild(square);
-
 };
