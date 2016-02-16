16abb13635e89c8ecf4a71c7ace76dbd7ee36169
Alan Taylor <alancarltaylor@gmail.com>
2016-02-15 20:12:14 -0700

    flashing now works

--- script.js
+++ script.js
@@ -3,40 +3,37 @@
   window.addEventListener("load", checkerBoard);
 
 function checkerBoard(){
+ window.setInterval (function(){
 
-
-
-  for (var i = 0; i <= 8; i ++){
-    function getColor1 (){
-      var color = "rgb(75, 0, "+ 20 * i +")"
-      return color;
+  function randomizer() {
+    var letters = '0123456789ABCDEF'.split('');
+    var color = '#';
+    for (var i = 0; i < 6; i++ ) {
+        color += letters[Math.floor(Math.random() * 16)];
+    }
+    return color;
     }
 
-    function getColor2 (){
-        var color = "rgb(30, 144, "+ 20 * i +")"
-        return color;
-      }
+  document.body.innerHTML="";
+    for (var i = 0; i <= 8; i ++){
 
-    for (var k = 0; k <= 8; k ++){
-
-
-      if ((i%2 != 0 || k%2 == 0) && (i%2 == 0 || k%2 !=0)){
-        var newDiv = document.createElement("div");
-        bodyTarget.appendChild(newDiv);
-        newDiv.style.backgroundColor = getColor1();
-        newDiv.style.width = "11.1%";
-        newDiv.style.paddingBottom = "11.1%"
-        newDiv.style.float = "left";
-      }else {
-        var newDiv = document.createElement("div");
-        bodyTarget.appendChild(newDiv);
-        newDiv.style.backgroundColor = getColor2();
-        newDiv.style.paddingBottom = "11.1%"
-        newDiv.style.width = "11.1%";
-        newDiv.style.float = "left";
-
-
+      for (var k = 0; k <= 8; k ++){
+        if ((i%2 != 0 || k%2 == 0) && (i%2 == 0 || k%2 !=0)){
+          var newDiv = document.createElement("div");
+          bodyTarget.appendChild(newDiv);
+          newDiv.style.backgroundColor = randomizer();
+          newDiv.style.width = "11.1%";
+          newDiv.style.paddingBottom = "11.1%"
+          newDiv.style.float = "left";
+        }else {
+          var newDiv = document.createElement("div");
+          bodyTarget.appendChild(newDiv);
+          newDiv.style.backgroundColor = randomizer();
+          newDiv.style.paddingBottom = "11.1%"
+          newDiv.style.width = "11.1%";
+          newDiv.style.float = "left";
+          }
       }
     }
-  }
+  }, 2000);
 }
