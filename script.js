b36727aaefb930b0ffddfde5e8ee15e3299fac09
Brad Butterfield <butters5789@gmail.com>
2016-02-16 01:29:53 -0700

    gradiant

--- script.js
+++ script.js
@@ -1,10 +1,19 @@
 // Your JS goes here
 var body = document.getElementsByTagName('body')[0];
+var container = document.createElement('div');
+body.insertBefore(container, body.firstChild);
+
 for (i = 0; i < 99; i++) {
   var div1 = document.createElement('div');
-  body.insertBefore(div1, body.firstChild);
-  div1.setAttribute("style", "background-color: red; width: 11.1%; float: left; padding: 0 0 11.1% 0");
+  container.appendChild(div1);
+  div1.style.background = 'rgb(' + (99-i) + ',' + (i*4) +', 255)';
+  div1.style.width = '11.1%';
+  div1.style.float = 'left';
+  div1.style.padding = '0 0 11.1% 0';
   var div2 = document.createElement('div');
-  body.insertBefore(div2, body.firstChild);
-  div2.setAttribute("style", "background-color: black; width: 11.1%; float: left; padding: 0 0 11.1% 0");
+  container.appendChild(div2);
+  div2.style.background = 'rgb(' + (99+(i*3)) + ', 0, 255)';
+  div2.style.width = '11.1%';
+  div2.style.float = 'left';
+  div2.style.padding = '0 0 11.1% 0';
 }
