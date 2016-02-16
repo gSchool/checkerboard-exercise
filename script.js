8ddb63f74c6618a919698694f501082e32ae4ed2
Brad Butterfield <butters5789@gmail.com>
2016-02-15 23:10:59 -0700

    random colors

--- script.js
+++ script.js
@@ -3,8 +3,14 @@ var body = document.getElementsByTagName('body')[0];
 for (i = 0; i < 99; i++) {
   var div1 = document.createElement('div');
   body.insertBefore(div1, body.firstChild);
-  div1.setAttribute("style", "background-color: red; width: 11.1%; float: left; padding: 0 0 11.1% 0");
+  div1.style.backgroundColor = '#'+Math.random().toString(16).substr(-6);
+  div1.style.width = '11.1%';
+  div1.style.float = 'left';
+  div1.style.padding = '0 0 11.1% 0';
   var div2 = document.createElement('div');
   body.insertBefore(div2, body.firstChild);
-  div2.setAttribute("style", "background-color: black; width: 11.1%; float: left; padding: 0 0 11.1% 0");
+  div2.style.backgroundColor = '#'+Math.random().toString(16).substr(-6);
+  div2.style.width = '11.1%';
+  div2.style.float = 'left';
+  div2.style.padding = '0 0 11.1% 0';
 }
