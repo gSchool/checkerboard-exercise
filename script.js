6bf64c08b5e8ef31a9978ca2d33470db5bf9726e
Summer Wollin <summer.wollin@gmail.com>
2016-02-15 22:41:24 -0700

    Include solution for checkerboard style

--- script.js
+++ script.js
@@ -1,1 +1,14 @@
-// Your JS goes here
+var body = document.getElementsByTagName('body')[0];
+for (i = 0; i <= 197; i++) {
+  var newDiv = document.createElement('div');
+  body.appendChild(newDiv);
+  newDiv.style.width = '11.1%';
+  newDiv.style.float = 'left';
+  newDiv.style.paddingBottom = '11.1%';
+  newDiv.style.backgroundColor = (i%2) ? 'black' : 'red';
+}
+
+document.getElementsByTagName('html')[0].style.height = '100%';
+document.getElementsByTagName('html')[0].style.margin = '0';
+body.style.height = '100%';
+body.style.margin = '0';
