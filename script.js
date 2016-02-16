d99d9a4f4fee247b40d1f58e3213a28d10bd232d
Daniel Stewart <daniel.robbins.stewart@gmail.com>
2016-02-15 17:39:40 -0700

    Add gradient

--- script.js
+++ script.js
@@ -1,5 +1,4 @@
-// Your JS goes here
-
+// // Your JS goes here
 for(i=0; i<9; i++) {
   if(i % 2 === 1) {
     for(x=0; x<9 ; x++) {
@@ -11,10 +10,12 @@ for(i=0; i<9; i++) {
       newTile.style.paddingBottom = "11.1%";
       newTile.style.float = "left";
       if(x % 2 === 0) {
-        newTile.style.backgroundColor = "red";
+        newTile.style.backgroundColor = "blue";
+        newTile.style.opacity = "0.4";
       }
       if(x % 2 === 1) {
-        newTile.style.backgroundColor = "black";
+        newTile.style.backgroundColor = "pink";
+        newTile.style.opacity = "0.4";
       }
     }
   }
@@ -28,11 +29,17 @@ for(i=0; i<9; i++) {
       newTile.style.paddingBottom = "11.1%";
       newTile.style.float = "left";
       if(x % 2 === 0) {
-        newTile.style.backgroundColor = "black";
+        newTile.style.backgroundColor = "pink";
+        newTile.style.opacity = "0.4";
       }
       if(x % 2 === 1) {
-        newTile.style.backgroundColor = "red";
+        newTile.style.backgroundColor = "blue";
+        newTile.style.opacity = "0.4";
       }
     }
   }
 }
+
+var body = document.querySelector('body');
+body.style.marginTop = "0%";
+body.style.backgroundImage = "linear-gradient(blue, red)";
