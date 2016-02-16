e3a182bf1cd2101a2776d863cee06cdd8ed8c4d8
James Freeman <freeman.james.h@gmail.com>
2016-02-15 20:37:58 -0700

    red/black checkerboard complete

--- script.js
+++ script.js
@@ -1,1 +1,30 @@
-// Your JS goes here
+for (var i = 0; i < 99; i++){
+
+if (i % 2 === 0){
+
+var divvy = document.createElement('div');
+divvy.style.backgroundColor = "black";
+divvy.style.position = "relative";
+divvy.style.float = "left"
+divvy.style.width = "11.1%";
+divvy.style.paddingBottom = "11.1%";
+divvy.className = "evens"
+
+
+} else {
+
+var divvy = document.createElement('div');
+divvy.style.backgroundColor = "red";
+divvy.style.position = "relative";
+divvy.style.float = "left";
+divvy.style.width = "11.1%";
+divvy.style.paddingBottom = "11.1%";
+divvy.className = "odds"
+
+
+
+}
+
+document.body.appendChild(divvy);
+
+}
