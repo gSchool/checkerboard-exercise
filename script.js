992f89a1a71c51bdb5c7e01613c44dea5580ed8e
Alan Taylor <alancarltaylor@gmail.com>
2016-02-15 18:12:36 -0700

    looks cleaner

--- script.js
+++ script.js
@@ -4,25 +4,6 @@
 
 function checkerBoard(){
 
-  // var newTable = document.createElement("table");
-  // var newTableRow = document.createElement("tr");
-  // var newTableData = document.createElement("td");
-  //
-  // newTableRow.appendChild(newTableData);
-  // newTable.appendChild(newTableRow);
-  // bodyTarget.appendChild(newTable);
-  //
-  // newTable.style.backgroundColor("blue");
-
-
-  // var newDiv = document.createElement("div");
-  // bodyTarget.appendChild(newDiv);
-  // newDiv.style.height = "140px";
-  // newDiv.style.width = "11.1%";
-  // newDiv.style.backgroundColor = "black";
-  // newDiv.syle.float = "left";
-
-
   for (var i = 0; i <= 8; i ++){
     for (var k = 0; k <= 8; k ++){
       if ((i%2 != 0 || k%2 == 0) && (i%2 == 0 || k%2 !=0)){
@@ -44,29 +25,4 @@ function checkerBoard(){
       }
     }
   }
-
-    //var body = document.getElementsByTagName('body')[0];
-    // var tbl = document.createElement('table');
-    // tbl.style.width = '100%';
-    // tbl.style.height = '500px';
-    // tbl.setAttribute('border', '1px');
-    // var tbdy = document.createElement('tbody');
-    // for (var i = 0; i < 9; i++) {
-    //     var tr = document.createElement('tr');
-    //     for (var j = 0; j < 9; j++) {
-    //         if (i == 8 && j == 8) {
-    //             break
-    //         } else {
-    //             var td = document.createElement('td');
-    //             //td.appendChild(document.createTextNode('\u0020'))
-    //             //i == 1 && j == 1 ? td.setAttribute('rowSpan', '2') : null;
-    //             tr.appendChild(td)
-    //         }
-    //     }
-    //     tbdy.appendChild(tr);
-    // }
-    // tbl.appendChild(tbdy);
-    // bodyTarget.appendChild(tbl)
-
-
 }
