0cf120004872f1d67549e5af567520b61a7b2a54
Daniel Stewart <daniel.robbins.stewart@gmail.com>
2016-02-15 17:53:36 -0700

    All tiles flashing random colors

--- script.js
+++ script.js
@@ -1,18 +1,18 @@
 // Your JS goes here
 
-for(i=0; i<9; i++) {
-  for(x=0; x<9 ; x++) {
-    var newTile = document.createElement('div');
-    var body = document.getElementsByTagName('body');
-    document.body.appendChild(newTile);
-    newTile.style.width = "11.1%";
-    newTile.style.paddingBottom = "11.1%";
-    newTile.style.float = "left";
-    newTile.style.backgroundColor = '#'+ Math.floor(Math.random() * 16777215).toString(16);
-  }
+for(i = 0; i < 80; i++) {
+  var newTile = document.createElement('div');
+  var body = document.getElementsByTagName('body');
+  document.body.appendChild(newTile);
+  newTile.style.width = "11.1%";
+  newTile.style.paddingBottom = "11.1%";
+  newTile.style.float = "left";
+  newTile.style.backgroundColor = '#'+ Math.floor(Math.random() * 16777215).toString(16);
 }
 
-var flashingTiles = setInterval(function() {
- var newTile = document.querySelector('div');
- newTile.style.backgroundColor = '#'+Math.floor(Math.random() *16777215).toString(16);
- }, 1000);
+setInterval(function() {
+  for(i = 0; i < 80; i++) {
+   var newTile = document.getElementsByTagName('div')[i];
+   newTile.style.backgroundColor = '#'+Math.floor(Math.random() *16777215).toString(16);
+ }
+}, 1000);
