31def2329af907d990da84e331921f7267bad5df
Lincoln Dennis <j.lincoln.dennis@gmail.com>
2016-02-15 20:47:39 -0700

    Refactor random colors, add flashing

--- script.js
+++ script.js
@@ -1,20 +1,32 @@
-for ( var i = 0; i < 81; i++ ) {
+function randomColor () {
+  var r = (Math.floor(Math.random() * 255));
+  var g = (Math.floor(Math.random() * 255));
+  var b = (Math.floor(Math.random() * 255));
+  var randColor = 'rgba(' +r+ ', ' +  g + ', ' + b  + ', ' + (Math.random()) + ')';
+  return randColor;
+}
 
- var body = document.getElementsByTagName('body')[0];
+(function (){
+  for (var i = 0; i < 81; i++) {
+    var body = document.getElementsByTagName('body')[0];
 
-  body.style.margin= "0";
+    body.style.margin = "0";
 
-  var square = document.createElement('div');
+    var square = document.createElement('div');
 
-  square.style.width = "11.1%";
-  square.style.float = "left";
-  square.style.paddingBottom = "11.1%";
-  body.style.background = 'linear-gradient(to bottom right, red, green)';
+    square.style.display = "inline-block";
+    square.style.width = "11.1%";
+    square.style.float = "left";
+    square.style.paddingBottom = "11.1%";
+    square.style.backgroundColor = randomColor();
 
-  if( i % 2 == 0 ) {
-    square.style.backgroundColor = '';
-  } else {
-    square.style.background = 'blue';
-  }
-  body.appendChild(square);
-};
+    body.appendChild(square);
+  };
+})();
+
+setInterval(function() {
+  for (var i = 0; i < 81; i++) {
+    var newDiv = document.getElementsByTagName('div')[i];
+    newDiv.style.backgroundColor = randomColor();
+  };
+}, 2000);
