65551cdbf535d0601557e0f75d34361b5a21b115
Brad Butterfield <butters5789@gmail.com>
2016-02-16 23:29:34 -0700

    flashing

--- script.js
+++ script.js
@@ -1,10 +1,28 @@
 // Your JS goes here
 var body = document.getElementsByTagName('body')[0];
+var container = document.createElement('section');
+body.insertBefore(container, body.firstChild);
+
 for (i = 0; i < 99; i++) {
   var div1 = document.createElement('div');
-  body.insertBefore(div1, body.firstChild);
-  div1.setAttribute("style", "background-color: red; width: 11.1%; float: left; padding: 0 0 11.1% 0");
+  container.appendChild(div1);
+  div1.style.backgroundColor = '#'+Math.random().toString(16).substr(-6);
+  div1.style.width = '11.1%';
+  div1.style.float = 'left';
+  div1.style.padding = '0 0 11.1% 0';
   var div2 = document.createElement('div');
-  body.insertBefore(div2, body.firstChild);
-  div2.setAttribute("style", "background-color: black; width: 11.1%; float: left; padding: 0 0 11.1% 0");
+  container.appendChild(div2);
+  div2.style.backgroundColor = '#'+Math.random().toString(16).substr(-6);
+  div2.style.width = '11.1%';
+  div2.style.float = 'left';
+  div2.style.padding = '0 0 11.1% 0';
 }
+
+function flashing() {
+    for (i = 0; i < 198; i++) {
+      var x = document.getElementsByTagName('div')[i];
+      x.style.backgroundColor = '#'+Math.random().toString(16).substr(-6);
+    }
+}
+
+setInterval(flashing, 2000)
