4249b775321157d172c3373263a884ce2eb7cc9a
Summer Wollin <summer.wollin@gmail.com>
2016-02-17 19:42:25 -0700

    Include solution for gradient

--- script.js
+++ script.js
@@ -1,11 +1,12 @@
 var body = document.getElementsByTagName('body')[0];
-for (i = 0; i <= 197; i++) {
+for (i = 0; i <= 89; i++) {
   var newDiv = document.createElement('div');
+  var currentBlue = 0;
   body.appendChild(newDiv);
   newDiv.style.width = '11.1%';
   newDiv.style.float = 'left';
   newDiv.style.paddingBottom = '11.1%';
-  newDiv.style.backgroundColor = (i%2) ? 'black' : 'red';
+  newDiv.style.backgroundColor = (i%2) ? "rgb(0,0,"+(i).toString()+")" : "rgb(100,0,"+(i).toString()+")";
 }
 
 document.getElementsByTagName('html')[0].style.height = '100%';
