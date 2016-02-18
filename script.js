80ce9c77f88bfce4b278427ba0302da61455daa7
MSturges <Sturges3232@gmail.com>
2016-02-17 21:09:25 -0700

    solution

--- script.js
+++ script.js
@@ -1,1 +1,27 @@
 // Your JS goes here
+
+
+//Not My Work.
+
+setInterval(function() {
+  var body = document.querySelector('body');
+
+  while (body.hasChildNodes())
+  // may your child, be a masculine child - godfather
+  body.removeChild(body.lastChild);
+
+  for (var i = 0; i < 72; i++) {
+    body.style.margin = '0px';
+
+    var randomColor = 'rgb(' + Math.floor(Math.random() * (255)) + ', ' + Math.floor(Math.random() * (255)) + ', ' + Math.floor(Math.random() * (255)) + ')';
+
+    var square = document.createElement('div');
+    square.style.width = '11.1%';
+    square.style.float = 'left';
+    square.style.paddingBottom = '11.1%';
+    square.style.backgroundColor = randomColor;
+
+    body.appendChild(square);
+  }
+
+}, 1000);
