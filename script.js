0132d24c9a8be819bb8d7fb405a1a2c8a45628e5
Ryan Sobol <contact@ryansobol.com>
2016-02-23 08:52:37 -0800

    Add solution

--- script.js
+++ script.js
@@ -1,1 +1,19 @@
-// Your JS goes here
+'use strict';
+
+var body = document.getElementsByTagName('body')[0];
+
+for (var i = 0; i < 63; i++) {
+  var div = document.createElement('div');
+
+  div.style.float = 'left';
+  div.style.width = '11.1%';
+  div.style.paddingBottom = '11.1%';
+
+  if (i % 2 === 0) {
+    div.style.backgroundColor = 'red';
+  } else {
+    div.style.backgroundColor = 'black';
+  }
+
+  body.appendChild(div);
+}
