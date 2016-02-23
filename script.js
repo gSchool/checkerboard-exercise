a05f4a570812d57c2798585d9377d7c5554296e8
LionelLints <lionel.lints@gmail.com>
2016-02-23 08:47:14 -0800

    add solution to gradient

--- script.js
+++ script.js
@@ -1,1 +1,16 @@
 // Your JS goes here
+//gradient:
+var body = document.getElementsByTagName('body')[0];
+body.style.background = "linear-gradient(135deg, red, blue)";
+for (var i = 1; i <= 63; i++) {
+  let div = document.createElement('div');
+  div.style.float = "left";
+  div.style.width = "11.1%";
+  div.style.paddingBottom = "11.1%";
+  if (i % 2 === 0) {
+    div.style.backgroundColor = "rgba(173, 33, 99, " + (.02 * i) + ")";
+  } else {
+    div.style.backgroundColor = "rgba(13, 133, 199, " + (.02 * i) + ")";
+  }
+  body.appendChild(div);
+}
