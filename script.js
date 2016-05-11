551fd11dffbff31338cef08f82ac81b568d7da37
Roy <rechase84@gmail.com>
2016-05-11 17:17:54 -0600

    working solution for filling a screen with red/black tiles

--- script.js
+++ script.js
@@ -4,7 +4,6 @@
 // var body = document.getElementsByTagName('body');
 
 var checkBoard = document.createElement('div');
-  checkBoard.height = "900px";
   checkBoard.width = "100%";
 
 var red = document.createElement('div');
@@ -19,18 +18,19 @@ var black = document.createElement('div');
   black.style.float = "left";
   black.style.paddingBottom = "11.1%";
 
+//create an object which can be reused over and over again on the DOM
+//initialize first two children
 document.body.appendChild(checkBoard);
 checkBoard.appendChild(red);
 checkBoard.appendChild(black);
-checkBoard.appendChild(red);
-checkBoard.appendChild(black);
-checkBoard.appendChild(red);
-checkBoard.appendChild(black);
-checkBoard.appendChild(red);
-checkBoard.appendChild(black);
-checkBoard.appendChild(red);
-checkBoard.appendChild(black);
-checkBoard.appendChild(red);
-checkBoard.appendChild(black);
-checkBoard.appendChild(red);
-checkBoard.appendChild(black);
+//increment i each time
+for (var i = 1; i < 51; i++) {
+  //initialize my clones with the distinctive of i
+  var redClone = red.cloneNode(i);
+  var blackClone = black.cloneNode(i);
+
+  //push the distinctive clones to the checkBoard node
+  checkBoard.appendChild(redClone);
+  checkBoard.appendChild(blackClone);
+
+}
