6bbcb571a7ba1a0438e972920ae8bdc05223292f
Kristen Foster-Marks <Kristenfoster-marks@Kristens-Macbrook-Pro.local>
2016-05-12 10:24:11 -0700

    finished gradient

--- script.js
+++ script.js
@@ -8,33 +8,50 @@ blackBox.setAttribute('class', 'odd');
 var redBox = document.createElement('div');
 redBox.setAttribute('class', 'even');
 
-//how to accomplish populating board with a for-loop?
-//use querySelector('containerDiv').length in an if statement; if # is fewer than 81, create variable and append child
-
-function randomRGB () {
-	var number = Math.floor(Math.random() * 255) + 1;
-	return number;
-}
-
-function randomColor() {
-  var r = randomRGB();
-  var g = randomRGB();
-  var b = randomRGB();
-  return 'rgb(' + r + ',' + g + ',' + b + ')';
-}
+// function randomRGB () {
+// 	var number = Math.floor(Math.random() * 255) + 1;
+// 	return number;
+// }
+//
+// function randomColor() {
+//   var r = randomRGB();
+//   var g = randomRGB();
+//   var b = randomRGB();
+//   return 'rgb(' + r + ',' + g + ',' + b + ')';
+// }
 
 
 var numberOfBoxes = document.querySelectorAll('.odd').length + document.querySelectorAll('.even').length;
-console.log(numberOfBoxes);
+
+//blackBox rgb variables
+var rOdd = 255;
+var gOdd = 255;
+var bOdd = 100;
+
+//evenBox variables
+var rEven = 255;
+var gEven = 100;
+var bEven = 255;
+
+//console.log(rgb);
+
 while (numberOfBoxes < 81) {
-  var blackBoxClone = blackBox.cloneNode(true);
+	var blackBoxClone = blackBox.cloneNode(true);
   containerDiv.appendChild(blackBoxClone);
-  blackBoxClone.style.backgroundColor = randomColor();
+	var rgbOdd = 'rgb(' + rOdd + ',' + gOdd + ',' + bOdd + ')';
+  blackBoxClone.style.backgroundColor = rgbOdd;
+	rOdd -= 3;
+	gOdd -= 3
+	console.log(rOdd + "," + gOdd);
   numberOfBoxes += 1;
   if (numberOfBoxes < 81) {
     var redBoxClone = redBox.cloneNode(true);
     containerDiv.appendChild(redBoxClone);
-    redBoxClone.style.backgroundColor = randomColor();
+		var rgbEven = 'rgb(' + rEven + ',' + gEven + ',' + bEven + ')';
+    redBoxClone.style.backgroundColor = rgbEven;
+		rEven -= 3
+		gEven -= 3;
+		console.log(rEven + "," + gEven);
     numberOfBoxes += 1;
   }
 }
