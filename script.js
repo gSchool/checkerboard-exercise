ff3a459f1c262a73a1d3d77b26bca9570e85b6d5
Ed <ek2000@gmail.com>
2016-05-24 16:31:48 -0500

    adds solution

--- script.js
+++ script.js
@@ -1,1 +1,15 @@
 // Your JS goes here
+
+
+for(var x = 0; x < 63; x++){
+  var div = document.createElement("div");
+  div.style.float = "left";
+  div.style.width = "11.1%";
+  div.style.paddingBottom = "11.1%";
+  if(x % 2 === 0){
+    div.style.backgroundColor = "red";
+  }else{
+    div.style.backgroundColor = "black";
+  }
+  document.body.appendChild(div);
+}
