13c234fa06c63252bc88016d04f18c35c0f19c2c
Devin Stone <devstone11@gmail.com>
2016-06-02 16:44:33 -0600

    Add random color grid

--- script.js
+++ script.js
@@ -6,6 +6,14 @@ var body = document.getElementsByTagName("body");
     box.style.width = "11.1%";
     box.style.paddingBottom = "11.1%";
     box.style.float = "left";
+    var characters = "0123456789ABCDEF"
+    var hex = function() {
+      var arr = [];
+      for (var i = 0; i < 6; i++) {
+        arr.push(characters[Math.floor(Math.random()*16)]);
+      }
+      return "#" + arr.join('');
+    }
+    box.style.backgroundColor = hex();
     body[0].appendChild(box);
-    (i%2 === 0) ? box.style.backgroundColor = "black" : box.style.backgroundColor = "red";
   };
