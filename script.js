27d4bc730ca6bbdd37c4e610b0d6be904c307f79
Tom <tomhendersonfl@gmail.com>
2016-06-02 15:44:52 -0600

    basic checkerboard

--- script.js
+++ script.js
@@ -1,1 +1,5 @@
-// Your JS goes here
+var el="";
+for (var i = 1; i < 65; i++) {
+  el += `<div style="width: 11.1%; float:left; padding-bottom: 11.1%; background-color: ${i%2 === 0 ? "red" : "black"}"></div>`;
+}
+document.getElementsByTagName("body")[0].innerHTML = el;
