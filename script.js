fc0f4689d3497a1199185400ef3724fdd66f5708
Ricky Padilla <rickycpadilla@gmail.com>
2016-06-02 15:53:47 -0600

    Made squares random colors

--- script.js
+++ script.js
@@ -9,16 +9,20 @@ body.appendChild(container);
 
 for(var i=0; i<41; i++){
   var block1 = document.createElement("div");
-  block1.style.backgroundColor = "black";
+  var ranColor1 = "#" + (Math.random() * 1000000).toFixed(0);
+  block1.style.backgroundColor = ranColor1;
   block1.style.height = "11.1%";
   block1.style.width = "11.1%";
   block1.style.float = "left";
   container.appendChild(block1);
   if (i==40){break};
   var block2 = document.createElement("div");
-  block2.style.backgroundColor = "red";
+  var ranColor2 = "#" + (Math.random() * 1000000).toFixed(0);
+  block2.style.backgroundColor = ranColor2;
   block2.style.height = "11.1%";
   block2.style.width = "11.1%";
   block2.style.float = "left";
   container.appendChild(block2);
 }
+
+console.log("#" + (Math.random() * 1000000));
