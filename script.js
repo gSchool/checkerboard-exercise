e4d0c8d4b9d73e9f74b149020d19389ecabc4822
Scott Wilkins <scottwilkins@MacBook-Air.local>
2016-06-02 17:43:20 -0600

    add checkerboard solutions

--- script.js
+++ script.js
@@ -1,1 +1,20 @@
 // Your JS goes here
+document.addEventListener("DOMContentLoaded",function(){
+  var theBody          = document.getElementsByTagName('body')[0];
+  theBody.style.height = "100vh";
+  theBody.style.width  = "100vh";
+  for (var i = 0; i <= 62; i++) {
+    var checker          = document.createElement('div');
+    checker.style.height = "11.1%";
+    checker.style.width  = "11.1%";
+    checker.style.float  = "left";
+    if(i % 2 === 0) {
+      checker.style.backgroundColor = "black";
+      theBody.appendChild(checker)
+    } else {
+      checker.style.backgroundColor = "red";
+      theBody.appendChild(checker)
+    }
+  }
+
+})
