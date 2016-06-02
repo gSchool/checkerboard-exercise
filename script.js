fb43f6a113dbf40eec2d275e2c853fb335e9d82b
Devin Stone <devstone11@gmail.com>
2016-06-02 17:52:42 -0600

    Add gradient

--- script.js
+++ script.js
@@ -1,19 +1,20 @@
 // Your JS goes here
 var body = document.getElementsByTagName("body");
+body[0].style.backgroundImage = "linear-gradient(45deg, #3441FB, white)";
 
-  for (var i = 0; i < 81; i++) {
-    var box = document.createElement("div");
-    box.style.width = "11.1%";
-    box.style.paddingBottom = "11.1%";
-    box.style.float = "left";
-    var characters = "0123456789ABCDEF"
-    var hex = function() {
-      var arr = [];
-      for (var i = 0; i < 6; i++) {
-        arr.push(characters[Math.floor(Math.random()*16)]);
-      }
-      return "#" + arr.join('');
-    }
-    box.style.backgroundColor = hex();
-    body[0].appendChild(box);
-  };
+  // for (var i = 0; i < 81; i++) {
+  //   var box = document.createElement("div");
+  //   box.style.width = "11.1%";
+  //   box.style.paddingBottom = "11.1%";
+  //   box.style.float = "left";
+  //   // var characters = "0123456789ABCDEF"
+  //   // var hex = function() {
+  //   //   var arr = [];
+  //   //   for (var i = 0; i < 6; i++) {
+  //   //     arr.push(characters[Math.floor(Math.random()*16)]);
+  //   //   }
+  //   //   return "#" + arr.join('');
+  //   // }
+  //   // box.style.backgroundColor = hex();
+  //   body[0].appendChild(box);
+  // };
