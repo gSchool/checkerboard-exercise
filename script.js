4ffeee6ca866e042e2d10f136a20b7e75d5eae13
Jim Watkins <JimWatkins@Jims-MacBook-Pro.local>
2016-06-02 13:25:50 -0600

    red/black checkerboard complete

--- script.js
+++ script.js
@@ -1,1 +1,12 @@
-// Your JS goes here
+var addToBody = document.getElementsByTagName('body')[0];
+addToBody.style.height = "100vh";
+addToBody.style.width = "100vh";
+
+for (var i = 0; i <= 62; i++) {
+  var box = document.createElement('div');
+  box.style.width = "11.1%";
+  box.style.float = "left";
+  box.style.paddingBottom = "11.1%";
+  (i % 2 === 0) ? box.style.backgroundColor = "black" : box.style.backgroundColor = "red";
+  addToBody.appendChild(box);
+};
