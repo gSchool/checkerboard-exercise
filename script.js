4025dc84ffed10c9d7386b5637cb4a11872a3fe6
cole sobel <colesobel@coles-MacBook-Air.local>
2016-06-02 16:05:13 -0600

    random color generator checkerboard complete

--- script.js
+++ script.js
@@ -6,41 +6,32 @@ container.style.width = '700px'
 
 body.appendChild(container)
 
+var hex = '1 2 3 4 5 6 7 8 9 0 a b c d e f'.split(' ')
+// console.log(Math.random);
+// console.log(hex[Math.floor(Math.random * hex.length)]);
 
+function randomGen() {
+    var color = hex[Math.floor(Math.random() * hex.length).toString()] +
+        hex[Math.floor(Math.random() * hex.length).toString()] +
+        hex[Math.floor(Math.random() * hex.length).toString()] +
+        hex[Math.floor(Math.random() * hex.length).toString()] +
+        hex[Math.floor(Math.random() * hex.length).toString()] +
+        hex[Math.floor(Math.random() * hex.length).toString()];
+
+    color = '#' + color
+    return color
+}
+
+console.log(randomGen());
 
 
 for (var i = 0; i < 9; i++) {
-    if (i % 2 === 0) {
         for (var j = 0; j < 9; j++) {
             var newDiv = document.createElement('div')
             newDiv.style.height = '11.1%'
             newDiv.style.width = '11.1%'
             newDiv.style.float = 'left'
+            newDiv.style.backgroundColor = randomGen()
             container.appendChild(newDiv)
-            if (j % 2 === 0) {
-                newDiv.style.backgroundColor = 'black'
-            }
-            else {
-                newDiv.style.backgroundColor = 'red'
-            }
-
         }
-    }
-    else {
-        for (var k = 0; k < 9; k++) {
-            var newDiv = document.createElement('div')
-            newDiv.style.height = '11.1%'
-            newDiv.style.width = '11.1%'
-            newDiv.style.float = 'left'
-            container.appendChild(newDiv)
-            if (k % 2 !== 0) {
-                newDiv.style.backgroundColor = 'black'
-            }
-            else {
-                newDiv.style.backgroundColor = 'red'
-            }
-        }
-
-
-    }
 }
