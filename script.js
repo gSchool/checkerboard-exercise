0aefbf3fa5af6960b59354859ec0b89bf8e08a95
Jim Watkins <JimWatkins@Jims-MacBook-Pro.local>
2016-06-02 13:58:29 -0600

    finished random color checkerboard

--- script.js
+++ script.js
@@ -3,10 +3,14 @@ addToBody.style.height = "100vh";
 addToBody.style.width = "100vh";
 
 for (var i = 0; i <= 62; i++) {
+  var randomNumber1 = Math.floor(Math.random()*256);
+  var randomNumber2 = Math.floor(Math.random()*256);
+  var randomNumber3 = Math.floor(Math.random()*256);
   var box = document.createElement('div');
+  var colorId = `rgb(${randomNumber1}, ${randomNumber2}, ${randomNumber3})`;
   box.style.width = "11.1%";
   box.style.float = "left";
   box.style.paddingBottom = "11.1%";
-  (i % 2 === 0) ? box.style.backgroundColor = "black" : box.style.backgroundColor = "red";
+  box.style.backgroundColor = colorId;
   addToBody.appendChild(box);
 };
