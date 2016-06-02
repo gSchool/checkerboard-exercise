afee55006864bfde242e4b86f3e5367bf32c9683
Jim Watkins <JimWatkins@Jims-MacBook-Pro.local>
2016-06-02 14:37:27 -0600

    finished gradient checkerboard

--- script.js
+++ script.js
@@ -2,16 +2,18 @@ var addToBody = document.getElementsByTagName('body')[0];
 addToBody.style.height = "100vh";
 addToBody.style.width = "100vh";
 
+var randomNumber1 = Math.floor(Math.random()*256);
+var randomNumber2 = Math.floor(Math.random()*256);
+var randomNumber3 = Math.floor(Math.random()*256);
+var colorId = `rgb(${randomNumber1}, ${randomNumber2}, ${randomNumber3})`;
+var colorId2 = `rgb(${randomNumber3}, ${randomNumber2}, ${randomNumber1})`;
+
 for (var i = 0; i <= 62; i++) {
-  var randomNumber1 = Math.floor(Math.random()*256);
-  var randomNumber2 = Math.floor(Math.random()*256);
-  var randomNumber3 = Math.floor(Math.random()*256);
   var box = document.createElement('div');
-  var colorId = `rgb(${randomNumber1}, ${randomNumber2}, ${randomNumber3})`;
-  var colorId2 = `rgb(${randomNumber3}, ${randomNumber2}, ${randomNumber1})`;
   box.style.width = "11.1%";
   box.style.float = "left";
   box.style.paddingBottom = "11.1%";
-  box.style.background = `linear-gradient(to bottom right, ${colorId}, ${colorId2})`;
+  box.style.opacity = i*.016;
+  (i % 2 === 0) ? box.style.backgroundColor = colorId : box.style.backgroundColor = colorId2;
   addToBody.appendChild(box);
 };
