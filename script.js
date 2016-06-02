7e2cd7d7d0951ea1646fdb5ca8e687b6d8bae7f3
Tom <tomhendersonfl@gmail.com>
2016-06-02 16:22:57 -0600

    random colors

--- script.js
+++ script.js
@@ -1,5 +1,9 @@
-var el="";
-for (var i = 1; i < 65; i++) {
-  el += `<div style="width: 11.1%; float:left; padding-bottom: 11.1%; background-color: ${i%2 === 0 ? "red" : "black"}"></div>`;
+var el=document.getElementsByTagName("body")[0].innerHTML;
+for (var i = 1; i < 64; i++) {
+  var randomColor = "#";
+  for (var j = 0; j < 3; j++) {
+    randomColor += ("0" + (Math.random()*256|0).toString(16)).substr(-2);
+  }
+  el += `<div style="width: 11.1%; float:left; padding-bottom: 11.1%; background-color: ${randomColor}"></div>`;
 }
 document.getElementsByTagName("body")[0].innerHTML = el;
