4b616fdf1d9c29294cfa5f25914d494d853a3f09
Ricky Padilla <rickycpadilla@gmail.com>
2016-06-02 16:11:41 -0600

    added gradient feature

--- script.js
+++ script.js
@@ -7,16 +7,22 @@ container.style.backgroundColor = "gray";
 container.style.margin = "-8px";
 body.appendChild(container);
 
-for(var i=0; i<41; i++){
+for(var i=10; i<102; i+=2){
   var block1 = document.createElement("div");
-  block1.style.backgroundColor = "black";
+  var color1 = "#33cc"
+  color1 += i;
+  console.log(color1);
+  block1.style.backgroundColor = color1;
   block1.style.height = "11.1%";
   block1.style.width = "11.1%";
   block1.style.float = "left";
   container.appendChild(block1);
-  if (i==40){break};
+  if (i==101){break};
   var block2 = document.createElement("div");
-  block2.style.backgroundColor = "red";
+  var color2 = "#ffff"
+  color2 += i;
+  console.log(color2);
+  block2.style.backgroundColor = color2;
   block2.style.height = "11.1%";
   block2.style.width = "11.1%";
   block2.style.float = "left";
