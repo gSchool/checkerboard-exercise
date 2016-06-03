2241fcf9ab71445b238a674fafa0b97b8c25ffb4
Jerry Benton Walden Goebel <jbwg312@Jerrys-MacBook-Pro.local>
2016-06-03 11:06:57 -0600

    flashing completed with interval fart noise

--- script.js
+++ script.js
@@ -4,7 +4,6 @@ var div1 = document.createElement("div");
 var div2 = document.createElement("div");
 var div3 = document.createElement("div");
 
-body.style.background = rando();
 
 div1.id = "container";
 div1.style.boxSizing = "border-box";
@@ -12,32 +11,44 @@ div1.style.margin = "-10px -10px 0px -10px";
 body.appendChild(div1);
 
 var container = document.getElementById("container");
+setInterval(makeBoard, 2000);
+setInterval(fart, 1500);
 
+function fart(){
+  var div = document.createElement("audio");
+  div.src = "Fart Sound Effect.mp3";
+  div.autoplay = true;
+  body.appendChild(div);
+}
 
-var op = 1;
-var col1 = rando();
-var col2 = rando();
-for(var i = 0; i < 81; i++){
-    if(i % 2){
-      var div_even = document.createElement("div");
-      div_even.style.background = col1;
-      div_even.style.width = "11.1%";
-      div_even.style.height = "11.1vw";
-      div_even.style.display = "inline-block";
-      div_even.style.float = "left";
-      div_even.style.opacity = (op -= 0.016).toString();
+function makeBoard(){
+  body.style.background = rando();
+  container.innerHTML = "";
+    var op = 1;
+    var col1 = rando();
+    var col2 = rando();
+    for(var i = 0; i < 81; i++){
+        if(i % 2){
+          var div_even = document.createElement("div");
+          div_even.style.background = col1;
+          div_even.style.width = "11.1%";
+          div_even.style.height = "11.1vw";
+          div_even.style.display = "inline-block";
+          div_even.style.float = "left";
+          div_even.style.opacity = (op -= 0.016).toString();
 
-      container.appendChild(div_even)
-    } else {
-      var div_odd = document.createElement("div");
-      div_odd.style.background = col2;
-      div_odd.style.width = "11.1%";
-      div_odd.style.height = "11.1vw";
-      div_odd.style.display = "inline-block";
-      div_odd.style.float = "left";
-      div_odd.style.opacity = (op -= 0.016);
+          container.appendChild(div_even)
+        } else {
+          var div_odd = document.createElement("div");
+          div_odd.style.background = col2;
+          div_odd.style.width = "11.1%";
+          div_odd.style.height = "11.1vw";
+          div_odd.style.display = "inline-block";
+          div_odd.style.float = "left";
+          div_odd.style.opacity = (op -= 0.016);
 
-      container.appendChild(div_odd)
+          container.appendChild(div_odd)
+        }
   }
 }
 
