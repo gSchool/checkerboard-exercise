4be9d3117d259c089d3441f5afe0659ef5685867
Kendra Lyndon <kendralyndon@gmail.com>
2016-06-03 11:06:31 -0600

    add color randomizer

--- script.js
+++ script.js
@@ -1,10 +1,10 @@
 // Your JS goes here
 document.addEventListener('DOMContentLoaded',function(){
   document.body.style.margin = '-8px';
-  for(i = 0; i < 81; i ++){
+  for(var i = 0; i < 81; i++){
     var div = document.createElement('div');
     addDivStyle(div);
-    colorRandom(div);
+    div.style.backgroundColor = makeHexaDec();
     document.body.appendChild(div);
   }
 })
@@ -15,25 +15,14 @@ function addDivStyle(div){
   div.style.paddingBottom = '11.1%';
 }
 
-function colorBlack(div){
-  div.style.backgroundColor = 'black';
-}
-
-function colorRed(div){
-  div.style.backgroundColor = 'red';
-}
-
-function colorRandom(div){
-  var hexaDec = makeHexaDec();
-  div.style.backgroundColor = hexaDec;
-}
-
 function makeHexaDec(){
   var hexaDec = '#';
-  for(i = 0; i < 6; i ++){
+  for(var i = 0; i < 6; i++){
     var number = Math.floor(Math.random()*15);
+    console.log(number);
     var choices = ['1','2','3','4','5','6','7','8','9','a','b','c','d','e','f'];
     var char = choices[number];
     hexaDec += char;
-  }; return hexaDec;
+  }
+  return hexaDec;
 }
