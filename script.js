567c6e5ea7e660a2552af777c7a268df8b9c1999
Michael Tobin <mikel.tobin@gmail.com>
2016-06-03 09:57:00 -0600

    reworked the function to be more fluent and less constrained by hardcoding of the boxes. Can now create more universal box styling. Also added 0 padding and margin to the body.

--- script.js
+++ script.js
@@ -1,24 +1,21 @@
-// Your JS goes here
-
 var bodyToAppend = document.getElementsByTagName('body')[0];
 var newArr = [];
-
+bodyToAppend.style.padding = '0';
+bodyToAppend.style.margin = '0';
 function createBoxes(){
-  var newBlackBox = document.createElement('div');
-  var newRedBox = document.createElement('div');
-  newBlackBox.style.backgroundColor = '#000000';
-  newBlackBox.style.width = '11.1%';
-  newBlackBox.style.height = '11.1%';
-  newBlackBox.style.float = 'left';
-  newBlackBox.style.paddingBottom = '11.1%';
-  newRedBox.style.backgroundColor = 'red';
-  newRedBox.style.width = '11.1%';
-  newRedBox.style.height = '11.1%';
-  newRedBox.style.float = 'left';
-  newRedBox.style.paddingBottom = '11.1%';
-  bodyToAppend.appendChild(newBlackBox);
-  bodyToAppend.appendChild(newRedBox);
+  for (var i = 0; i < 80; i++) {
+    var newBox = [];
+    newBox[i] = document.createElement('div');
+    newBox[i].style.width = '11.1%';
+    newBox[i].style.height = '11.1%';
+    newBox[i].style.float = 'left';
+    newBox[i].style.paddingBottom = '11.1%';
+    if(i%2 === 0){
+      newBox[i].style.backgroundColor = '#000000';
+    }else {
+      newBox[i].style.backgroundColor ='red';
+    }
+    bodyToAppend.appendChild(newBox[i]);
+  }
 }
-for (var i = 0; i < 32; i++) {
-  createBoxes();
-}
+createBoxes();
