ec20e47186d0726bc84f055cce87e11a418dbbc4
Michael Tobin <mikel.tobin@gmail.com>
2016-06-03 09:54:19 -0600

    Wrong branch, save progress to this branch. This RandomBranch will be overwritten next push

--- script.js
+++ script.js
@@ -1,1 +1,26 @@
 // Your JS goes here
+var bodyToAppend = document.getElementsByTagName('body')[0];
+var newArr = [];
+
+function createBoxes(){
+  for (var i = 0; i < 80; i++) {
+    var newBox = [];
+    newBox[i] = document.createElement('div');
+    newBox[i].style.width = '11.1%';
+    newBox[i].style.height = '11.1%';
+    newBox[i].style.float = 'left';
+    newBox[i].style.paddingBottom = '11.1%';
+    if(i%2 === 0){
+      console.log('work');
+      newBox[i].style.backgroundColor = '#000000';
+    }else {
+      newBox[i].style.backgroundColor ='red';
+    }
+    bodyToAppend.appendChild(newBox[i]);
+  }
+  // var newBlackBox = document.createElement('div');
+  // var newRedBox = document.createElement('div');
+
+}
+
+createBoxes();
