fe644cbcc242316fdc19366579212e0c6cdfd084
cole sobel <colesobel@coles-MacBook-Air.local>
2016-06-02 18:25:24 -0600

    flashing checker board working

--- script.js
+++ script.js
@@ -6,45 +6,32 @@ container.style.width = '700px'
 
 body.appendChild(container)
 
-var blueVal = 30
-var greenVal = 92
+var hex = '1 2 3 4 5 6 7 8 9 0 a b c d e f'.split(' ')
+
+function randomGen() {
+    var color = hex[Math.floor(Math.random() * hex.length).toString()] +
+        hex[Math.floor(Math.random() * hex.length).toString()] +
+        hex[Math.floor(Math.random() * hex.length).toString()] +
+        hex[Math.floor(Math.random() * hex.length).toString()] +
+        hex[Math.floor(Math.random() * hex.length).toString()] +
+        hex[Math.floor(Math.random() * hex.length).toString()];
+
+    color = '#' + color
+    return color
+}
 
 
 
-for (var i = 0; i < 9; i++) {
-    if (i % 2 === 0) {
+setInterval(function() {
+    container.innerHTML = ''
+    for (var i = 0; i < 9; i++) {
         for (var j = 0; j < 9; j++) {
             var newDiv = document.createElement('div')
             newDiv.style.height = '11.1%'
             newDiv.style.width = '11.1%'
             newDiv.style.float = 'left'
+            newDiv.style.backgroundColor = randomGen()
             container.appendChild(newDiv)
-            if (j % 2 === 0) {
-                newDiv.style.backgroundColor = 'rgb(' + greenVal + ',190,30)'
-            }
-            else {
-                newDiv.style.backgroundColor = 'rgb(' + blueVal + ',49,190)'
-            }
-
         }
     }
-    else {
-        for (var k = 0; k < 9; k++) {
-            var newDiv = document.createElement('div')
-            newDiv.style.height = '11.1%'
-            newDiv.style.width = '11.1%'
-            newDiv.style.float = 'left'
-            container.appendChild(newDiv)
-            if (k % 2 !== 0) {
-                newDiv.style.backgroundColor = 'rgb(' + greenVal + ',190,30)'
-            }
-            else {
-                newDiv.style.backgroundColor = 'rgb(' + blueVal + ',49,190)'
-            }
-        }
-
-
-    }
-    blueVal += 25
-    greenVal += 25
-}
+}, 2000)
