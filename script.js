7ba7801caddbbf7bf8a1f2cf3c490b90d7cd37ff
Sarah Healey <s.deane.healey@gmail.com>
2016-06-03 14:03:35 -0600

    paddingBottom for red and black squares were updated

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 // Your JS goes here
 
-var num = 8;
+
 var body = document.getElementsByTagName("body");
 
 function blackStart() {
@@ -8,18 +8,20 @@ for (var i=0; i<9; i++) {
   if(i%2===0) {
   var tileBlack = document.createElement("div");
   tileBlack.style.width = "11.1%";
-  tileBlack.style.height = "160px";
+  tileBlack.style.height = "11.1%";
   tileBlack.style.background = "black";
   tileBlack.style.color = "white";
   tileBlack.style.float = "left";
+  tileBlack.style.paddingBottom = "11.1%";
   document.body.appendChild(tileBlack);
 } else {
     var tileRed = document.createElement("div");
     tileRed.style.width = "11.1%";
-    tileRed.style.height = "160px";
+    tileRed.style.height = "11.1%";
     tileRed.style.background = "red";
     tileRed.style.color = "white";
     tileRed.style.float = "left";
+    tileRed.style.paddingBottom = "11.1%";
     document.body.appendChild(tileRed);
 }
 }
@@ -30,18 +32,20 @@ for (var i=0; i<9; i++) {
   if (i%2 === 0) {
     var tileRed = document.createElement("div");
     tileRed.style.width = "11.1%";
-    tileRed.style.height = "160px";
+    tileRed.style.height = "11.1%";
     tileRed.style.background = "red";
     tileRed.style.color = "white";
     tileRed.style.float = "left";
+    tileRed.style.paddingBottom = "11.1%";
     document.body.appendChild(tileRed);
   } else {
     var tileBlack = document.createElement("div");
     tileBlack.style.width = "11.1%";
-    tileBlack.style.height = "160px";
+    tileBlack.style.height = "11.1%";
     tileBlack.style.background = "black";
     tileBlack.style.color = "white";
     tileBlack.style.float = "left";
+    tileBlack.style.paddingBottom = "11.1%";
     document.body.appendChild(tileBlack);
   }
 }
@@ -54,17 +58,3 @@ for (var i=0; i<5; i++) {
     redStart();
   }
 }
-// } else {
-//   var tileRed = document.createElement("div");
-//   tileRed.style.width = "11.1%";
-//   tileRed.style.height = "100px";
-//   tileRed.style.background = "red";
-//   tileRed.style.color = "white";
-//   tileRed.style.float = "left";
-//   tileRed.innerHTML = "Hello";
-//   document.body.appendChild(tileRed);
-// }
-// Each tile should be a `div`
-// * Each tile's width is `11.1%`
-// * Set each tile's `float` property to `left`
-// * Each tile's paddingBottom is `11.1%`
