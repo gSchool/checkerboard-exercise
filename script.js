89232ee3f30fe43972ab6b9fe8396d06102c6a7e
Michael Tobin <mikel.tobin@gmail.com>
2016-06-03 09:46:13 -0600

    Finished Part 1. Red and black boxes are now checkered across the string, using only JS for the style and html injection. Going to rework it to satisfy a broader range of color changing and box creation

--- script.js
+++ script.js
@@ -1,1 +1,29 @@
 // Your JS goes here
+
+var bodyToAppend = document.getElementsByTagName('body')[0];
+var newArr = [];
+
+function createBoxes(){
+  var newBlackBox = document.createElement('div');
+  var newRedBox = document.createElement('div');
+  newBlackBox.style.backgroundColor = '#000000';
+  newBlackBox.style.width = '11.1%';
+  newBlackBox.style.height = '11.1%';
+  newBlackBox.style.float = 'left';
+  newBlackBox.style.paddingBottom = '11.1%';
+  newRedBox.style.backgroundColor = 'red';
+  newRedBox.style.width = '11.1%';
+  newRedBox.style.height = '11.1%';
+  newRedBox.style.float = 'left';
+  newRedBox.style.paddingBottom = '11.1%';
+  bodyToAppend.appendChild(newBlackBox);
+  bodyToAppend.appendChild(newRedBox);
+}
+for (var i = 0; i < 32; i++) {
+  createBoxes();
+}
+
+// Each tile should be a div
+// Each tile's width is 11.1%
+// Set each tile's float property to left
+// Each tile's paddingBottom is 11.1%
