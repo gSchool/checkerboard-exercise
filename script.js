2468823fb9859ee69794f7630431edc4b1fe4048
Adriana Galvez <vntgh@yahoo.com>
2016-06-02 21:25:38 -0600

    Flashing

--- script.js
+++ script.js
@@ -1,25 +1,28 @@
 // Your JS goes here
-var body = document.getElementsByTagName("body")[0];
-var grad = document.createElement("div");
-grad.style.background="linear-gradient(red,blue,black)";
-grad.style.width = "100%";
-grad.style.height="925px";
-grad.style.position = "absolute";
-grad.style.opacity = "0.6";
-body.appendChild(grad);
-
-for (var i = 0; i < 27; i++) {
-  var bblock = document.createElement("div");
-  var rblock = document.createElement("div");
-  bblock.style.backgroundColor = "green";
-  bblock.style.width = "11.1%";
-  bblock.style.cssFloat = "left";
-  bblock.style.paddingBottom = "11.1%";
-  rblock.style.backgroundColor = "black";
-  rblock.style.width = "11.1%";
-  rblock.style.cssFloat = "left";
-  rblock.style.paddingBottom = "11.1%";
-
-  body.appendChild(rblock);
-  body.appendChild(bblock);
+function random() {
+    var x = [1,2,3,4,5,6,7,8,9,0,"A","B","C","D","E","F"];
+    var color = '#';
+    for (var i = 0; i < 6; i++ ) {
+        color += x[Math.floor(Math.random() * 16)];
+    }
+    return color;
 }
+
+var body = document.getElementsByTagName("body")[0];
+for (var i = 0; i < 45; i++) {
+  var block = document.createElement("div");
+  block.style.backgroundColor = random();
+  block.style.width = "11.1%";
+  block.style.cssFloat = "left";
+  block.style.paddingBottom = "11.1%";
+  body.appendChild(block);
+}
+var blocks = document.getElementsByTagName("div");
+
+function change(){
+  for (var b in blocks){
+    blocks[b].style.backgroundColor = random();
+  }
+}
+
+setInterval('change()', 2000);
