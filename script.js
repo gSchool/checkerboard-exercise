dc581fb776ee5511258995c04ad17af363613696
Devin Stone <devstone11@gmail.com>
2016-06-03 09:37:40 -0600

    Finish Flashing Checkerboard

--- script.js
+++ script.js
@@ -1,19 +1,27 @@
 // Your JS goes here
 var body = document.getElementsByTagName("body");
+var divs = document.getElementsByTagName("div");
+var characters = "0123456789ABCDEF";
 
-  for (var i = 0; i < 81; i++) {
-    var box = document.createElement("div");
-    box.style.width = "11.1%";
-    box.style.paddingBottom = "11.1%";
-    box.style.float = "left";
-    var characters = "0123456789ABCDEF"
-    var hex = function() {
-      var arr = [];
-      for (var i = 0; i < 6; i++) {
-        arr.push(characters[Math.floor(Math.random()*16)]);
-      }
-      return "#" + arr.join('');
-    }
-    box.style.backgroundColor = hex();
-    body[0].appendChild(box);
-  };
+var hex = function() {
+  var arr = [];
+  for (var i = 0; i < 6; i++) {
+    arr.push(characters[Math.floor(Math.random()*16)]);
+  }
+  return "#" + arr.join('');
+}
+
+for (var i = 0; i < 81; i++) {
+  var box = document.createElement("div");
+  box.style.width = "11.1%";
+  box.style.paddingBottom = "11.1%";
+  box.style.float = "left";
+  box.style.backgroundColor = hex();
+  body[0].appendChild(box);
+};
+
+setInterval(function(){
+  for (var i = 0; i < divs.length; i++) {
+    divs[i].style.backgroundColor = hex();
+  }
+}, 2000);
