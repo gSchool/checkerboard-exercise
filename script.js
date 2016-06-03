13a8f369b6e020f4cf483093fc0dedbfa4b54a58
Tom <tomhendersonfl@gmail.com>
2016-06-02 23:07:08 -0600

    gradient

--- script.js
+++ script.js
@@ -1,9 +1,9 @@
 var el=document.getElementsByTagName("body")[0].innerHTML;
 for (var i = 1; i < 64; i++) {
-  var randomColor = "#";
-  for (var j = 0; j < 3; j++) {
-    randomColor += ("0" + (Math.random()*256|0).toString(16)).substr(-2);
-  }
-  el += `<div style="width: 11.1%; float:left; padding-bottom: 11.1%; background-color: ${randomColor}"></div>`;
+  el += `<div style="width: 11.1%; float:left; padding-bottom: 11.1%; background-color: ${i%2 === 0 ? "red" : "black"}"></div>`;
 }
+el += `</section>`;
 document.getElementsByTagName("body")[0].innerHTML = el;
+
+//; background: linear-gradient(135deg, red, blue)
+// ${i%2 === 0 ? red : blue}
