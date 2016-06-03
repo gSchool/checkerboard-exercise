685abaaa3aa7996f89b5df2085672a1c8963aa6d
Sarah <sarahmichellemeyer@gmail.com>
2016-06-03 10:45:58 -0600

    gradient colors

--- script.js
+++ script.js
@@ -3,18 +3,25 @@ container.style.width = '700px';
 container.style.height = '700px';
 document.body.appendChild(container);
 
-setInterval(function() {
-  container.innerHTML = '';
-  for (var i = 0; i < 81; i++) {
+
+  for (var i =10; i < 91; i++) {
+    if(i%2 === 0) {
+      var checker = document.createElement('div');
+        var color = '#9900';
+        color += i;
+        checker.style.backgroundColor = color;
+        checker.style.float = 'left';
+        checker.style.width = '11.1%';
+        checker.style.height = '11.1%';
+        container.appendChild(checker);
+} else {
   var checker = document.createElement('div');
-  function randomColor(){
-    return Math.floor(Math.random()*16777215).toString(16);
-  }
-  var checkerColor = ('#' + randomColor());
-  checker.style.backgroundColor = checkerColor;
+  var color2 = '#0099';
+  color2 += i;
+  checker.style.backgroundColor = color2;
   checker.style.float = 'left';
   checker.style.width = '11.1%';
   checker.style.height = '11.1%';
   container.appendChild(checker);
+  }
 }
-}, 2000);
