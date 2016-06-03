134a1353b7347af505155064b465be999d4d684a
Tom <tomhendersonfl@gmail.com>
2016-06-03 09:29:34 -0600

    WIP

--- script.js
+++ script.js
@@ -1,9 +1,55 @@
-var el=document.getElementsByTagName("body")[0].innerHTML;
-for (var i = 1; i < 64; i++) {
+//
+// var el=document.getElementsByTagName("body")[0].innerHTML;
+// for (var i = 1; i < 64; i++) {
+//   el += `<div style="width: 11.1%; float:left; padding-bottom: 11.1%; background-color: ${i%2 === 0 ? "red" : "black"}"></div>`;
+// }
+// el += `</section>`;
+// document.getElementsByTagName("body")[0].innerHTML = el;
+var el="";
+for (var i = 1; i < 65; i++) {
   el += `<div style="width: 11.1%; float:left; padding-bottom: 11.1%; background-color: ${i%2 === 0 ? "red" : "black"}"></div>`;
 }
-el += `</section>`;
 document.getElementsByTagName("body")[0].innerHTML = el;
 
+//
 //; background: linear-gradient(135deg, red, blue)
 // ${i%2 === 0 ? red : blue}
+
+// var el=document.getElementsByTagName("body")[0].innerHTML;
+// for (var i = 1; i < 64; i++) {
+//   var randomColor = "#";
+//   for (var j = 0; j < 3; j++) {
+//     randomColor += ("0" + (Math.random()*256|0).toString(16)).substr(-2);
+//   }
+//   el += `<div style="width: 11.1%; float:left; padding-bottom: 11.1%; background-color: ${randomColor}"></div>`;
+// }
+// document.getElementsByTagName("body")[0].innerHTML = el;
+
+// var body = document.getElementsByTagName('body')[0];
+// for (var i = 0; i < 63; i++) {
+//   var div = document.createElement('div');
+//   div.style.float = 'left';
+//   div.style.width = '11.1%';
+//   div.style.paddingBottom = '11.1%';
+//   if (i % 2 === 0) {
+//     div.style.backgroundColor = 'red';
+//   } else {
+//     div.style.backgroundColor = 'black';
+//   }
+//   body.appendChild(div);
+// }
+
+// flashing random colors
+// var id=setInterval(flash,2000)
+// function flash() {
+//   // var el=document.getElementsByTagName("body")[0].innerHTML;
+//   var el="";
+//   for (var i = 1; i < 65; i++) {
+//     var randomColor = "#";
+//     for (var j = 0; j < 3; j++) {
+//       randomColor += ("0" + (Math.random()*256|0).toString(16)).substr(-2);
+//     }
+//     el += `<div style="width: 11.2%; float:left; padding-bottom: 11.2%; background-color: ${randomColor}"></div>`;
+//   }
+//   document.getElementsByTagName("body")[0].innerHTML = el;
+// }
