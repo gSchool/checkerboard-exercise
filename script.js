611a307eb28f46af0839fc79f678eae6ebf22db5
Alex <amurphy345@gmail.com>
2016-06-03 13:44:30 -0600

    random flashing complete

--- script.js
+++ script.js
@@ -16,3 +16,20 @@ for (var i = 0; i < 81; i++) {
   square.style.marginBottom = "-5px"
   document.getElementById('container').appendChild(square)
 }
+
+
+
+window.setInterval(function(){
+  document.getElementById('container').innerHTML = ""
+  for (var i = 0; i < 81; i++) {
+    var square = document.createElement('div')
+    square.innerHTML = ' '
+    square.style.width="100px"
+    square.style.height="100px"
+    square.style.display="inline-block"
+    square.style.backgroundColor = '#'+Math.random().toString(16).slice(2,8).toUpperCase();
+    square.style.padding = 0
+    square.style.marginBottom = "-5px"
+    document.getElementById('container').appendChild(square)
+  }
+}, 2000);
