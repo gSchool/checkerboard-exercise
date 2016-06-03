8fe9b87c6fbfed9ebe467f42adccd1a896d6af9e
Devin Stone <devstone11@gmail.com>
2016-06-03 09:23:20 -0600

    Finish Gradient Checkerboard

--- script.js
+++ script.js
@@ -1,20 +1,13 @@
 // Your JS goes here
 var body = document.getElementsByTagName("body");
-body[0].style.backgroundImage = "linear-gradient(45deg, #3441FB, white)";
+body[0].style.backgroundImage = "linear-gradient(45deg, white, #0000cc)";
 
-  // for (var i = 0; i < 81; i++) {
-  //   var box = document.createElement("div");
-  //   box.style.width = "11.1%";
-  //   box.style.paddingBottom = "11.1%";
-  //   box.style.float = "left";
-  //   // var characters = "0123456789ABCDEF"
-  //   // var hex = function() {
-  //   //   var arr = [];
-  //   //   for (var i = 0; i < 6; i++) {
-  //   //     arr.push(characters[Math.floor(Math.random()*16)]);
-  //   //   }
-  //   //   return "#" + arr.join('');
-  //   // }
-  //   // box.style.backgroundColor = hex();
-  //   body[0].appendChild(box);
-  // };
+for (var i = 0; i < 81; i++) {
+  var box = document.createElement("div");
+  box.style.width = "11.1%";
+  box.style.paddingBottom = "11.1%";
+  box.style.float = "left";
+  body[0].appendChild(box);
+  (i%2 === 0) ? box.style.backgroundColor = "#CF2DF9" : box.style.backgroundColor = "#3DCEF2";
+  box.style.opacity = "0.5";
+};
