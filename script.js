c405af38d3877a808c7b05afd0bc3e228c740b93
Ryan Garofalo <ryangarofalo100@gmail.com>
2016-06-03 14:51:31 -0600

    Very Elegant Solution

--- script.js
+++ script.js
@@ -1,1 +1,11 @@
-// Your JS goes here
+
+
+  for (var i = 0; i < 63; i++) {
+    var tile=document.createElement('div');
+    tile.style.width = '11.1%'
+    tile.style.float = 'left'
+    tile.style.paddingBottom = '11.1%'
+    document.body.appendChild(tile);
+
+  (i%2 === 0 ? tile.style.backgroundColor = 'red': tile.style.backgroundColor = 'black');
+    }
