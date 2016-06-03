88cfc4fdf8274cce97c4444b710c696e6f872660
Sarah Healey <s.deane.healey@gmail.com>
2016-06-03 15:03:57 -0600

    random colors checkerboard

--- test.js
+++ test.js
@@ -1,16 +1,18 @@
-var body = document.getElementsByTagName("body");
+var body = document.getElementsByTagName("body")[0];
 
-function randomColorRow() {
+function rowOfRandom() {
 for (var i=0; i<9; i++) {
   var tileRandomColor = document.createElement("div");
   var color = '#'+ Math.floor(Math.random()*16777215).toString(16);
   tileRandomColor.style.width = "11.1%";
   tileRandomColor.style.height = "11.1%";
-  tileRandomColor.style.background = "red";
-  tileRandomColor.style.color = "red";
+  tileRandomColor.style.background = color;
   tileRandomColor.style.float = "left";
   tileRandomColor.style.paddingBottom = "11.1%";
-  tileRandomColor.innerHTML = "hello";
   document.body.appendChild(tileRandomColor);
 }
+}
+
+for (var i=0; i<5; i++) {
+  rowOfRandom();
 }
