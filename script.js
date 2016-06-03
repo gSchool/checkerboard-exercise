a3f4220a33f70ae818aaf3bd3366930d330c9cf6
Ryan Garofalo <ryangarofalo100@gmail.com>
2016-06-03 15:29:34 -0600

    random color works

--- script.js
+++ script.js
@@ -1,11 +1,29 @@
 
 
-  for (var i = 0; i < 63; i++) {
-    var tile=document.createElement('div');
-    tile.style.width = '11.1%'
-    tile.style.float = 'left'
-    tile.style.paddingBottom = '11.1%'
-    document.body.appendChild(tile);
+  // for (var i = 0; i < 63; i++) {
+  //   var tile=document.createElement('div');
+  //   tile.style.width = '11.1%'
+  //   tile.style.float = 'left'
+  //   tile.style.paddingBottom = '11.1%'
+  //   document.body.appendChild(tile);
+  //
+  // (i%2 === 0 ? tile.style.backgroundColor = 'red': tile.style.backgroundColor = 'black');
+  //   }
 
-  (i%2 === 0 ? tile.style.backgroundColor = 'red': tile.style.backgroundColor = 'black');
+    for (var i = 0; i < 63; i++) {
+      var tile=document.createElement('div');
+      tile.style.width = '11.1%'
+      tile.style.float = 'left'
+      tile.style.paddingBottom = '11.1%'
+      tile.style.backgroundColor =getRandomColor()
+      document.body.appendChild(tile);
+      }
+
+      function getRandomColor() {
+    var letters = '0123456789ABCDEF'.split('');
+    var color = '#';
+    for (var i = 0; i < 6; i++ ) {
+        color += letters[Math.floor(Math.random() * 16)];
     }
+    return color;
+}
