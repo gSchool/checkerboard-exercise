8a05e7fcb200339205b09abc31eea0f4efb233e3
Ryan Garofalo <ryangarofalo100@gmail.com>
2016-06-03 16:26:28 -0600

    A borrowed solution for gradient

--- script.js
+++ script.js
@@ -10,20 +10,37 @@
   // (i%2 === 0 ? tile.style.backgroundColor = 'red': tile.style.backgroundColor = 'black');
   //   }
 
-    for (var i = 0; i < 63; i++) {
-      var tile=document.createElement('div');
-      tile.style.width = '11.1%'
-      tile.style.float = 'left'
-      tile.style.paddingBottom = '11.1%'
-      tile.style.backgroundColor =getRandomColor()
-      document.body.appendChild(tile);
-      }
 
-      function getRandomColor() {
-    var letters = '0123456789ABCDEF'.split('');
-    var color = '#';
-    for (var i = 0; i < 6; i++ ) {
-        color += letters[Math.floor(Math.random() * 16)];
-    }
-    return color;
+
+//     for (var i = 0; i < 63; i++) {
+//       var tile=document.createElement('div');
+//       tile.style.width = '11.1%'
+//       tile.style.float = 'left'
+//       tile.style.paddingBottom = '11.1%'
+//       tile.style.backgroundColor =getRandomColor()
+//       document.body.appendChild(tile);
+//       }
+//
+//       function getRandomColor() {
+//     var letters = '0123456789ABCDEF'.split('');
+//     var color = '#';
+//     for (var i = 0; i < 6; i++ ) {
+//         color += letters[Math.floor(Math.random() * 16)];
+//     }
+//     return color;
+// }
+
+var body = document.getElementsByTagName('body')[0];
+body.style.background = "linear-gradient(135deg, red, blue)";
+for (var i = 1; i <= 63; i++) {
+  let div = document.createElement('div');
+  div.style.float = "left";
+  div.style.width = "11.1%";
+  div.style.paddingBottom = "11.1%";
+  if (i % 2 === 0) {
+    div.style.backgroundColor = "rgba(173, 33, 99, " + (.02 * i) + ")";
+  } else {
+    div.style.backgroundColor = "rgba(13, 133, 199, " + (.02 * i) + ")";
+  }
+  body.appendChild(div);
 }
