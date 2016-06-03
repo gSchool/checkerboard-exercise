cfa58aa8b172626c0d05b99c399194438d1f6e15
Jerry Benton Walden Goebel <jbwg312@Jerrys-MacBook-Pro.local>
2016-06-03 09:39:49 -0600

    random color generator

--- script.js
+++ script.js
@@ -13,23 +13,21 @@ var container = document.getElementById("container");
 
 
 for(var i = 0; i < 81; i++){
-  if(i % 2){
     var div_even = document.createElement("div");
-    div_even.style.background = "red";
+    div_even.style.background = rando();
     div_even.style.width = "11.1%";
     div_even.style.height = "11.1vw";
     div_even.style.display = "inline-block";
     div_even.style.float = "left";
 
     container.appendChild(div_even)
-  } else {
-    var div_odd = document.createElement("div");
-    div_odd.style.background = "black";
-    div_odd.style.width = "11.1%";
-    div_odd.style.height = "11.1vw";
-    div_odd.style.display = "inline-block";
-    div_odd.style.float = "left";
 
-    container.appendChild(div_odd)
-  }
+}
+
+function rando(){
+  var r = Math.floor(Math.random()*256);
+  var g = Math.floor(Math.random()*256);
+  var b = Math.floor(Math.random()*256);
+  var rgb = "rgb(" + r + "," + g + "," + b +")";
+  return rgb
 }
