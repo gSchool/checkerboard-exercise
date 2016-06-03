481e70ffff79988c17aa53ebba54a406d11fb086
Kendra Lyndon <kendralyndon@gmail.com>
2016-06-03 13:36:04 -0600

    add gradient

--- script.js
+++ script.js
@@ -1,34 +1,31 @@
-// Your JS goes here
+var percent = '45';
+
 document.addEventListener('DOMContentLoaded',function(){
   document.body.style.margin = '-8px';
   for(var i = 0; i < 81; i++){
+    var colorA = 'hsl(201,100%,'+percent+'%)';
+    var colorB = 'hsl(153,100%,'+percent+'%)';
+    if(i>0 && i%9===0){
+      percent = makeLighter(percent);
+    }
     var div = document.createElement('div');
     addDivStyle(div);
-    div.style.backgroundColor = makeHexaDec();
+    if(i%2===0){
+      div.style.backgroundColor = colorA;
+    } else{
+      div.style.backgroundColor = colorB;
+    }
     document.body.appendChild(div);
   }
-  var intervalID = window.setInterval(function(){
-    var number = Math.floor(Math.random()*80);
-    for(var i = 1; i < 82 ; i++){
-      var div = document.body.children[i];
-      div.style.backgroundColor = makeHexaDec();
-    }
-  },2000);
 })
+
+function makeLighter(percent){
+  percent = (parseInt(percent)+5).toString();
+  return percent;
+}
 
 function addDivStyle(div){
   div.style.width = '11.1%';
   div.style.cssFloat = 'left';
   div.style.paddingBottom = '11.1%';
-}
-
-function makeHexaDec(){
-  var hexaDec = '#';
-  for(var i = 0; i < 6; i++){
-    var number = Math.floor(Math.random()*15);
-    var choices = ['1','2','3','4','5','6','7','8','9','a','b','c','d','e','f'];
-    var char = choices[number];
-    hexaDec += char;
-  }
-  return hexaDec;
 }
