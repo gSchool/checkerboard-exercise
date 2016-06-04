99b5b60a953c3cec4ec3cfaace129505ca087521
Michael Coulter <msc2086@gmail.com>
2016-06-04 11:19:42 -0600

    rando

--- script.js
+++ script.js
@@ -1,6 +1,13 @@
 document.addEventListener('DOMContentLoaded', function(){
   var empty=document.getElementsByTagName('body')[0]
-
+  function getRandomColor() {
+      var letters = '0123456789ABCDEF'.split('');
+      var color = '#';
+      for (var i = 0; i < 6; i++ ) {
+          color += letters[Math.floor(Math.random() * 16)];
+      }
+      return color;
+  }
   for (var i = 0; i < 63; i++) {
     var tile_i = document.createElement('div')
     function addBlackTile(){
@@ -8,13 +15,13 @@ document.addEventListener('DOMContentLoaded', function(){
       tile_i.style.width = "11.1%"
       tile_i.style.paddingBottom = "11.1%"
       tile_i.style.float = "left"
-      tile_i.style.backgroundColor = "black"
+      tile_i.style.backgroundColor = getRandomColor()
     }
     function addRedTile(){
       tile_i.style.width = "11.1%"
       tile_i.style.paddingBottom = "11.1%"
       tile_i.style.float = "left"
-      tile_i.style.backgroundColor = "red"
+      tile_i.style.backgroundColor = getRandomColor()
       empty.appendChild(tile_i)
     }
     if(i%2==0){
