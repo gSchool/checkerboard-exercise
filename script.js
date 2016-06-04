622f930c1ffceb0dcf1f9fc73173f31539410809
MIchael <michael.olson756@gmail.com>
2016-06-04 12:22:56 -0600

    First Problem Finished

--- script.js
+++ script.js
@@ -1,1 +1,36 @@
-// Your JS goes here
+// * Each tile should be a `div`
+// * Each tile's width is `11.1%`
+// * Set each tile's `float` property to `left`
+// * Each tile's paddingBottom is `11.1%`
+
+//There are 9 squares across the top and seven squares down = 63
+
+
+
+
+
+var body = document.getElementsByTagName('body')[0]
+
+for (var i = 0; i < 40; i++) {
+
+var div = document.createElement("div1")
+  div.style.backgroundColor = "black";
+  div.style.paddingBottom = "11.1%";
+  div.style.width = "11.1%";
+  div.style.float = "left";
+  body.appendChild(div)
+
+var div = document.createElement("div2")
+  div.style.backgroundColor = "red";
+  div.style.paddingBottom = "11.1%";
+  div.style.width = "11.1%";
+  div.style.float = "left";
+  body.appendChild(div)
+
+}
+var div = document.createElement("div3")
+  div.style.backgroundColor = "black";
+  div.style.paddingBottom = "11.1%";
+  div.style.width = "11.1%";
+  div.style.float = "left";
+  body.appendChild(div)
