7c476fdc41d0d1fdcffe39386f1dcf070b482e44
vince vahala <vincevahala@gmail.com>
2016-06-06 15:43:12 -0600

    random colors working

--- script.js
+++ script.js
@@ -1,18 +1,18 @@
 
 for (var i = 0; i < 81; i++) {
-  var blackTile = document.createElement('div');
-  var redTile = document.createElement('div');
-  if(i%2 === 0) {
-    blackTile.style.backgroundColor = 'black';
-    blackTile.style.width = '11.1%';
-    blackTile.style.float = 'left';
-    blackTile.style.paddingBottom = '11.1%';
-    document.body.appendChild(blackTile);
-  } else {
-    redTile.style.backgroundColor = 'red';
-    redTile.style.width = '11.1%';
-    redTile.style.float = 'left';
-    redTile.style.paddingBottom = '11.1%';
-    document.body.appendChild(redTile);
-  }
+  var tile1 = document.createElement('div');
+    tile1.style.backgroundColor = getRandomColor();
+    tile1.style.width = '11.1%';
+    tile1.style.float = 'left';
+    tile1.style.paddingBottom = '11.1%';
+    document.body.appendChild(tile1);
+}
+
+function getRandomColor() {
+    var letters = '0123456789ABCDEF'.split('');
+    var color = '#';
+    for (var i = 0; i < 6; i++ ) {
+        color += letters[Math.floor(Math.random() * 16)];
+    }
+    return color;
 }
