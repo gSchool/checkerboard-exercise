429920f63cda562e24b6ded7abee2caddfba5703
Michael Coulter <msc2086@gmail.com>
2016-06-06 15:23:50 -0600

    Coulter flashing answer

--- script.js
+++ script.js
@@ -1,32 +1,30 @@
 document.addEventListener('DOMContentLoaded', function(){
   var empty=document.getElementsByTagName('body')[0]
-  function getRandomColor() {
-      var letters = '0123456789ABCDEF'.split('');
-      var color = '#';
-      for (var i = 0; i < 6; i++ ) {
-          color += letters[Math.floor(Math.random() * 16)];
+
+
+
+function getRandomColor() {
+    var letters = '0123456789ABCDEF'.split('');
+    var color = '#';
+    for (var i = 0; i < 6; i++ ) {
+        color += letters[Math.floor(Math.random() * 16)];
       }
-      return color;
+    return color;
   }
+
+var tiles = setInterval(function(){
+  empty.innerHTML="";
   for (var i = 0; i < 63; i++) {
     var tile_i = document.createElement('div')
-    function addBlackTile(){
+    function addTile(){
       empty.appendChild(tile_i)
       tile_i.style.width = "11.1%"
       tile_i.style.paddingBottom = "11.1%"
       tile_i.style.float = "left"
       tile_i.style.backgroundColor = getRandomColor()
     }
-    function addRedTile(){
-      tile_i.style.width = "11.1%"
-      tile_i.style.paddingBottom = "11.1%"
-      tile_i.style.float = "left"
-      tile_i.style.backgroundColor = getRandomColor()
-      empty.appendChild(tile_i)
-    }
-    if(i%2==0){
-      addBlackTile()
-    }
-  else{addRedTile()}
-  }
-})
+  addTile()}
+}, 2000)
+
+var tiles;
+  })
