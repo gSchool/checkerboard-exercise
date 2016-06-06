f7b6a4a0222b376a16cd420d571ec9e7c8e8d3fa
vince vahala <vincevahala@gmail.com>
2016-06-06 15:23:55 -0600

    black n red checkerboard

--- script.js
+++ script.js
@@ -1,1 +1,18 @@
-// Your JS goes here
+
+for (var i = 0; i < 81; i++) {
+  var blackTile = document.createElement('div');
+  var redTile = document.createElement('div');
+  if(i%2 === 0) {
+    blackTile.style.backgroundColor = 'black';
+    blackTile.style.width = '11.1%';
+    blackTile.style.float = 'left';
+    blackTile.style.paddingBottom = '11.1%';
+    document.body.appendChild(blackTile);
+  } else {
+    redTile.style.backgroundColor = 'red';
+    redTile.style.width = '11.1%';
+    redTile.style.float = 'left';
+    redTile.style.paddingBottom = '11.1%';
+    document.body.appendChild(redTile);
+  }
+}
