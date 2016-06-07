08032b79be36b7c56989681499d560c0b3f95465
Julie DeMasters <juliekdemasters@gmail.com>
2016-06-06 19:04:08 -0600

    forgot the var in the for loop which once I fixed it allowed the hexvalue to work in the randomized color generator

--- script.js
+++ script.js
@@ -1,27 +1,27 @@
 
 divContainer = document.createElement('div');
 
-// function colorPicker (){
-//   var stri = 'ABCDEFGHIJKLMN'.split('') ;
-//   var color = '#';
-//   for (i=0; i<6; i++){
-//     color += stri[Math.floor(Math.random()*6)];
-//   }
-//   return color;
-// }
-
-function colorPicker(){
-  RGB1 = Math.random
-  RGB2 =
-  RB3 =
-Math.floor(Math.random()*3);
-
+function colorPicker (){
+  var stri = 'ABCDEFGHIJKLMN'.split('') ;
+  var color = '#';
+  for (var i=0; i<6; i++){
+    color += stri[Math.floor(Math.random()*6)];
+  }
+  return color;
 }
 
-for(i=0;i<81;i++){
+// function colorPicker(){
+//   RGB1 = Math.random
+//   RGB2 =
+//   RB3 =
+// Math.floor(Math.random()*3);
+//
+// }
+
+for(var i=0;i<81;i++){
 
     var divColor = document.createElement('div');
-    divColor.style.backgroundColor= colorPicker;
+    divColor.style.backgroundColor= colorPicker();
     divColor.style.width = '11.1%'
     divColor.style.paddingBottom = '11.1%';
     divColor.style.float = 'left';
