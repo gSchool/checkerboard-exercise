3cb8c1f214ce0fd557889640c8471ecb7225412f
Julie DeMasters <juliekdemasters@gmail.com>
2016-06-06 18:12:14 -0600

    problem one completed

--- script.js
+++ script.js
@@ -1,1 +1,36 @@
-// Your JS goes here
+//first, let's create a div container
+//then let's create a div for red
+//then let's create a div for black
+//then let's style the red div
+//then let's style the black div
+
+var divContainer = document.createElement('div');
+var divRed = document.createElement('div');
+var divBlack = document.createElement('div');
+// var body = document.getElementsbyTagName('body');
+
+
+for(i=0;i<81;i++){
+  if(i % 2 === 0){
+    // divRed.innerHTML = divRed;
+    var divColor = document.createElement('div');
+    divColor.style.backgroundColor= 'red';
+    divColor.style.width = '11.1%'
+    divColor.style.paddingBottom = '11.1%';
+    divColor.style.float = 'left';
+    console.log(divRed);
+    //append your new red div to your container div
+    document.getElementsByTagName('body')[0].appendChild(divColor);
+  } else{
+    // divContainer.innerHTML = divBlack;
+    var divColor = document.createElement('div')
+    divColor.style.backgroundColor= 'black';
+    divColor.style.width = '11.1%'
+    divColor.style.paddingBottom = '11.1%';
+    divColor.style.float = 'left';
+    // console.log(divRed);
+    //append your new red div to your container div
+    document.getElementsByTagName('body')[0].appendChild(divColor);
+  }
+  divContainer.appendChild.divRed;
+}
