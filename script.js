3b1cb86711e4a35c18cda36a839341e47b373d6f
Julie DeMasters <juliekdemasters@gmail.com>
2016-06-06 18:59:10 -0600

    halfway through the random color picker problem

--- script.js
+++ script.js
@@ -1,36 +1,31 @@
-//first, let's create a div container
-//then let's create a div for red
-//then let's create a div for black
-//then let's style the red div
-//then let's style the black div
 
-var divContainer = document.createElement('div');
-var divRed = document.createElement('div');
-var divBlack = document.createElement('div');
-// var body = document.getElementsbyTagName('body');
+divContainer = document.createElement('div');
 
+// function colorPicker (){
+//   var stri = 'ABCDEFGHIJKLMN'.split('') ;
+//   var color = '#';
+//   for (i=0; i<6; i++){
+//     color += stri[Math.floor(Math.random()*6)];
+//   }
+//   return color;
+// }
+
+function colorPicker(){
+  RGB1 = Math.random
+  RGB2 =
+  RB3 =
+Math.floor(Math.random()*3);
+
+}
 
 for(i=0;i<81;i++){
-  if(i % 2 === 0){
-    // divRed.innerHTML = divRed;
+
     var divColor = document.createElement('div');
-    divColor.style.backgroundColor= 'red';
+    divColor.style.backgroundColor= colorPicker;
     divColor.style.width = '11.1%'
     divColor.style.paddingBottom = '11.1%';
     divColor.style.float = 'left';
-    console.log(divRed);
-    //append your new red div to your container div
-    document.getElementsByTagName('body')[0].appendChild(divColor);
-  } else{
-    // divContainer.innerHTML = divBlack;
-    var divColor = document.createElement('div')
-    divColor.style.backgroundColor= 'black';
-    divColor.style.width = '11.1%'
-    divColor.style.paddingBottom = '11.1%';
-    divColor.style.float = 'left';
-    // console.log(divRed);
-    //append your new red div to your container div
+
     document.getElementsByTagName('body')[0].appendChild(divColor);
   }
-  divContainer.appendChild.divRed;
-}
+  divContainer.appendChild.divColor;
