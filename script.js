18a1acf71f0a4e4204b33b46217c51a84b146322
Chana Reynolds <chana777@gmail.com>
2016-11-03 17:26:28 -0600

    Made-black+red-board

--- script.js
+++ script.js
@@ -1,1 +1,17 @@
-// Your JS goes here
+$(document).ready(function() {
+
+    for (var i = 0; i < 31; i++) {
+
+
+        $('body').append('<div class="redBox"></div>');
+        $('body').append('<div class="blackBox"></div>');
+
+    }
+    $('body').append('<div class="redBox"></div>');
+    $('div').css('width', '11.1%').css('paddingBottom', '11.1%').css('float', 'left')
+    $('.redBox').css('background-color', 'red');
+    $('.blackBox').css('background-color', 'black');
+
+
+});
+// ('background-color', 'red').css
