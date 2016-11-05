4e1dd4533e406d63764d2a3afae300c948404c6e
ikobdk <ikobidike@gmail.com>
2016-11-05 00:29:43 -0600

    completed checkerboard

--- script.js
+++ script.js
@@ -1,1 +1,13 @@
-// Your JS goes here
+var dimension = 8;
+var percentage = 100/dimension;
+for(var i=0;i<dimension;i++){
+  for(var j=0;j<dimension;j++){
+    if((i+j)%2==0){
+      $("body").append('<div class="red"></div>');
+      $(".red").width(percentage + '%').css('paddingBottom',percentage + '%').css('background','red').css('float','left');
+    }else{
+      $("body").append('<div class="black"></div>');
+      $(".black").width(percentage + '%').css('paddingBottom',percentage + '%').css('background','black').css('float','left');
+    }
+  }
+}
