c398d177f76110c79344e9da35ebe787338569c1
Austin Edwards <austintedwards@gmail.com>
2016-12-11 08:59:26 -0700

    Checkboard complete

--- script.js
+++ script.js
@@ -1,1 +1,21 @@
 // Your JS goes here
+function checker(){
+
+  for (var i = 0; i < 81; i++) {
+    var div = document.createElement("div");
+    document.body.appendChild(div);
+    div.style.width = "11.1%";
+    div.style.paddingBottom = "11.1%";
+    div.style.float = 'left';
+
+    if (i%2===0){
+      div.className="black";
+      div.style.backgroundColor = "black";
+    }else{
+      div.className="red";
+      div.style.backgroundColor = "red";
+    }
+  }
+}
+
+checker();
