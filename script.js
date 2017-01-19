c78084a34ec6d1cd600be59486eccdea58817602
Sasha Berkowitz <sasha.b510@gmail.com>
2017-01-19 15:59:14 -0800

    finished red/black board

--- script.js
+++ script.js
@@ -1,1 +1,17 @@
-// Your JS goes here
+function createTile(color) {
+  var div = document.createElement("div");
+  div.style.width = '11.1%';
+  div.style.float = 'left';
+  div.style.paddingBottom = '11.1%';
+  div.style.background = color;
+  return div;
+}
+
+
+for (let i = 0; i < 63; i++) {
+  if (i % 2 === 0) {
+    document.body.appendChild(createTile('black'));
+  } else {
+    document.body.appendChild(createTile('red'));
+  }
+}
