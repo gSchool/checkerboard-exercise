428fb7381294138ae52d46e9ac41f3ce5908296a
Marlena Baker <baker.marlena@gmail.com>
2017-05-04 17:38:16 -0600

    completed exercise

--- script.js
+++ script.js
@@ -1,1 +1,15 @@
-// Your JS goes here
+function makeCheckerBoard(ammount){
+
+  for (var i = 0; i < ammount; i++) {
+    if (i % 2 !== 0) {
+        document.querySelector('body').appendChild(document.createElement('div')).setAttribute(
+   "style", "background: red; width: 11.1%; height: 100px; float: left; paddingBottom:11.1%;");
+    }
+    else if (i% 2 == 0){
+      document.querySelector('body').appendChild(document.createElement('div')).setAttribute(
+ "style", "background: black; width: 11.1%; height: 100px; float: left; paddingBottom:11.1%;");
+    }
+  }
+}
+
+makeCheckerBoard(100);
