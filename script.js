58d27706b5af8535fa964333c32a1be66b95322d
JAKEFELDMAN <jacob.h.feldman@gmail.com>
2017-05-25 12:18:28 -0600

    Add Random Color

--- script.js
+++ script.js
@@ -1,30 +1,22 @@
 // Your JS goes here
 var getBody = document.getElementsByTagName("body")
 
-function createBlackDiv() {
-  var createBlackDiv = document.createElement("div");
-  document.body.appendChild(createBlackDiv)
-  createBlackDiv.style.paddingBottom = "11.1%"
-  createBlackDiv.style.width = "11.1%"
-  createBlackDiv.style.backgroundColor = "black"
-  createBlackDiv.style.float = "left"
-
+function createRandomColor() {
+  let createRandomColorDiv = document.createElement("div");
+  let r = Math.floor(Math.random() * 256);
+  let g = Math.floor(Math.random() * 256);
+  let b = Math.floor(Math.random() * 256);
+  createRandomColorDiv.style.backgroundColor = `rgb(${r}, ${g}, ${b})`
+  document.body.appendChild(createRandomColorDiv)
+  createRandomColorDiv.style.paddingBottom = "11.1%"
+  createRandomColorDiv.style.width = "11.1%"
+  createRandomColorDiv.style.float = "left"
 }
-
-function createRedDiv() {
-  var createRedDiv = document.createElement("div");
-  document.body.appendChild(createRedDiv)
-  createRedDiv.style.paddingBottom = "11.1%"
-  createRedDiv.style.width = "11.1%"
-  createRedDiv.style.backgroundColor = "red"
-  createRedDiv.style.float = "left"
-}
-
 
 for (let x = 0; x < 81; x++) {
   if (x % 2 == 0) {
-    createBlackDiv();
+    createRandomColor();
   } else {
-    createRedDiv();
+    createRandomColor();
   }
 }
