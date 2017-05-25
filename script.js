deadc51130b62580d9a0e7f6785c86b5a818e0a8
JAKEFELDMAN <jacob.h.feldman@gmail.com>
2017-05-25 12:39:06 -0600

    Gradient Version

--- script.js
+++ script.js
@@ -1,12 +1,12 @@
 // Your JS goes here
-var getBody = document.getElementsByTagName("body")
+var getBody = document.getElementsByTagName("body");
 
 function createBlackDiv() {
   var createBlackDiv = document.createElement("div");
   document.body.appendChild(createBlackDiv)
   createBlackDiv.style.paddingBottom = "11.1%"
   createBlackDiv.style.width = "11.1%"
-  createBlackDiv.style.backgroundColor = "black"
+  createBlackDiv.style.background = "linear-gradient(rgba(255,0,0,0),rgba(255,123,0,1))";
   createBlackDiv.style.float = "left"
 
 }
@@ -16,10 +16,9 @@ function createRedDiv() {
   document.body.appendChild(createRedDiv)
   createRedDiv.style.paddingBottom = "11.1%"
   createRedDiv.style.width = "11.1%"
-  createRedDiv.style.backgroundColor = "red"
+  createRedDiv.style.background = "linear-gradient(rgba(0,123,255,0.5),rgba(255,255,0,1))";
   createRedDiv.style.float = "left"
 }
-
 
 for (let x = 0; x < 81; x++) {
   if (x % 2 == 0) {
