0d13d801bd0b8e396bedad413b5b30d8fe773da1
David Nelson Murray <linearsix@gmail.com>
2018-08-07 21:50:51 -0500

    hoping this branch business does not overwrite my files

--- script.js
+++ script.js
@@ -2,34 +2,47 @@
 document.addEventListener('DOMContentLoaded', () => {
 
     let colorToggle = 0;
-    let randomColor; //testing branch change
+    let randomColor1;
+    let randomColor2;
+    let randomColor3;
+    let randomColorString;
+    
     for (let j=0; j < 4; j++) {
         for (let i = 0; i < 8; i++) {
-            colorToggle ++;
+            // colorToggle ++;
+            randomColor1 = Math.floor(Math.random() * 256);
+            randomColor2 = Math.floor(Math.random() * 256);
+            randomColor3 = Math.floor(Math.random() * 256);
+            randomColorString = `${randomColor1}, ${randomColor2}, ${randomColor3}`
             const tile = document.createElement('div');
             document.body.appendChild(tile);
-            if (colorToggle % 2 === 0) {
-                tile.style.backgroundColor = 'red';
-            } else {
-                tile.style.backgroundColor = 'black';
-            };
+            // if (colorToggle % 2 === 0) {
+            //     tile.style.backgroundColor = 'red';
+            // } else {
+            //     tile.style.backgroundColor = 'black';
+            // };
+            tile.style.backgroundColor = `rgb(${randomColorString})`;
             tile.style.cssFloat = 'left';
             tile.style.width = '11.1%'
             tile.style.height = '100px';
             tile.style.margin = '1px';
             tile.style.borderColor = 'black'
-            // tile.textContent = colorToggle;
         };
         colorToggle -= 1;
         for (let i = 0; i < 8; i++) {
-            colorToggle ++;
+            // colorToggle ++;
+            randomColor1 = Math.floor(Math.random() * 256);
+            randomColor2 = Math.floor(Math.random() * 256);
+            randomColor3 = Math.floor(Math.random() * 256);
+            randomColorString = `${randomColor1}, ${randomColor2}, ${randomColor3}`
             const tile = document.createElement('div');
             document.body.appendChild(tile);
-            if (colorToggle % 2 === 0) {
-                tile.style.backgroundColor = 'red';
-            } else {
-                tile.style.backgroundColor = 'black';
-            };
+            // if (colorToggle % 2 === 0) {
+            //     // tile.style.backgroundColor = 'red';
+            // } else {
+            //     // tile.style.backgroundColor = 'black';
+            // };
+            tile.style.backgroundColor = `rgb(${randomColorString})`
             tile.style.cssFloat = 'left';
             tile.style.width = '11.1%'
             tile.style.height = '100px';
@@ -37,6 +50,6 @@ document.addEventListener('DOMContentLoaded', () => {
             tile.style.borderColor = 'black'
             // tile.textContent = colorToggle;
         };
-        colorToggle ++;
+        // colorToggle ++;
     };
 })
