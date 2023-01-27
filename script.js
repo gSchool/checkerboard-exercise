1c8a2cf7ae0a7e7a9065a1805aeaae7e3f8ac417
cedrick roseberry <crose275@gmail.com>
2023-01-26 22:01:25 -0800

    Red and Black Checkerboard

--- script.js
+++ script.js
@@ -1,1 +1,17 @@
-// Your JS goes here
+function checkerBoardExercise() {// Your JS goes here
+    for(let i = 0; i < 63; i++) {
+        let body = document.querySelector('body');
+        let div = document.createElement('div');
+        body.appendChild(div);
+        div.style.width = '11.1%';
+        div.style.float = 'left';
+        div.style.paddingBottom = '11.1%';
+        if(i % 2 ==0) {
+            div.style.backgroundColor = 'red'
+        } else {
+            div.style.backgroundColor = 'black'
+        }
+    }
+    
+}
+console.log(checkerBoardExercise())
