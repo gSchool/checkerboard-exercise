aa78f761dcbb5d33b72ddecc5ab855ec5dd530f9
dave-rob <98975807+dave-rob@users.noreply.github.com>
2023-11-14 21:25:25 -0700

    Update script.js
    Cleaned up

--- script.js
+++ script.js
@@ -18,30 +18,3 @@ function randomColor(){
     var b = Math.floor(Math.random()*256);
     return `rgb(${r}, ${g}, ${b})`;
 }
-
-// var tiles = document.querySelectorAll("div");
-// //var blackTiles = document.querySelectorAll("div:nth-of-type(even)");
-// //var redTiles = document.querySelectorAll("div:nth-of-type(odd)");
-
-// var row = 1;
-// var col = 0;
-// for (var i = 0; i < tiles.length; i++) {
-//   //console.log("row: " + row + "i : " + i+ "col: " + col);
-//   //if row is odd, want odd col red & if row is even, even col are red
-//   if (row % 2 == 1 && (i + 1) % 2 == 1 || row % 2 == 0 && (i + 1) % 2 == 0) {
-//     tiles[i].style.backgroundColor = "red";
-//     tiles[i].style.height = "12.5%";
-//     tiles[i].style.width = "12.5%";
-//   } else {
-//     tiles[i].style.backgroundColor = "black";
-//     tiles[i].style.height = "12.5%";
-//     tiles[i].style.width = "12.5%";
-//   }
-
-//   if (col % 7 == 0 && col != 0) {
-//     row++;
-//     col = 0;
-//   } else {
-//     col++;
-//   }
-// }  
