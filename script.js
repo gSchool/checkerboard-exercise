a08bf6fce17cfbf83174805abaaeb3635a932161
Dave <dlrobjr@gmail.com>
2023-11-14 20:23:29 -0700

    added random color function and assign it to each

--- script.js
+++ script.js
@@ -6,32 +6,42 @@ con.style.width = "800px";
 con.style.height = "800px";
 
 for (var i = 0; i<64; i++){
-    con.appendChild(document.createElement("div"));
+    var tile = con.appendChild(document.createElement("div"));
+    tile.style.height="12.5%";
+    tile.style.width="12.5%";
+    tile.style.backgroundColor = randomColor();
 }
 
-var tiles = document.querySelectorAll("div");
-//var blackTiles = document.querySelectorAll("div:nth-of-type(even)");
-//var redTiles = document.querySelectorAll("div:nth-of-type(odd)");
+function randomColor(){
+    var r = Math.floor(Math.random()*256);
+    var g = Math.floor(Math.random()*256);
+    var b = Math.floor(Math.random()*256);
+    return `rgb(${r}, ${g}, ${b})`;
+}
 
-var row = 1;
-var col = 0;
-for (var i = 0; i < tiles.length; i++) {
-  //console.log("row: " + row + "i : " + i+ "col: " + col);
-  //if row is odd, want odd col red & if row is even, even col are red
-  if (row % 2 == 1 && (i + 1) % 2 == 1 || row % 2 == 0 && (i + 1) % 2 == 0) {
-    tiles[i].style.backgroundColor = "red";
-    tiles[i].style.height = "12.5%";
-    tiles[i].style.width = "12.5%";
-  } else {
-    tiles[i].style.backgroundColor = "black";
-    tiles[i].style.height = "12.5%";
-    tiles[i].style.width = "12.5%";
-  }
+// var tiles = document.querySelectorAll("div");
+// //var blackTiles = document.querySelectorAll("div:nth-of-type(even)");
+// //var redTiles = document.querySelectorAll("div:nth-of-type(odd)");
 
-  if (col % 7 == 0 && col != 0) {
-    row++;
-    col = 0;
-  } else {
-    col++;
-  }
-}  
+// var row = 1;
+// var col = 0;
+// for (var i = 0; i < tiles.length; i++) {
+//   //console.log("row: " + row + "i : " + i+ "col: " + col);
+//   //if row is odd, want odd col red & if row is even, even col are red
+//   if (row % 2 == 1 && (i + 1) % 2 == 1 || row % 2 == 0 && (i + 1) % 2 == 0) {
+//     tiles[i].style.backgroundColor = "red";
+//     tiles[i].style.height = "12.5%";
+//     tiles[i].style.width = "12.5%";
+//   } else {
+//     tiles[i].style.backgroundColor = "black";
+//     tiles[i].style.height = "12.5%";
+//     tiles[i].style.width = "12.5%";
+//   }
+
+//   if (col % 7 == 0 && col != 0) {
+//     row++;
+//     col = 0;
+//   } else {
+//     col++;
+//   }
+// }  
